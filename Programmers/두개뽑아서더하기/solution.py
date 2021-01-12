b5e883639587e3d25fb53136d0556e3bf7482887
junbangg <wnstjr4620@naver.com>
2021-01-12 15:15:55 +0900

    better solution

--- Programmers/두개뽑아서더하기/solution.py
+++ Programmers/두개뽑아서더하기/solution.py
@@ -7,3 +7,13 @@ def solution(numbers):
                 if n not in answer:
                     answer.append(n)
     return sorted(answer)
+
+
+#better
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    return sorted(set(answer))
