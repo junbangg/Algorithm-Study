66359c1da2ea34a07759a0046feabac6b3cfff22
junbangg <wnstjr4620@naver.com>
2021-01-13 16:31:57 +0900

    2016년

---
+++ Programmers/2016년/solution.py
@@ -0,0 +1,6 @@
+def solution(a, b):
+    months = [31,29,31,30,31,30,31,31,30,31,30,31]
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    for i in range(a-1):
+        b += months[i]
+    return days[(b%7)-1]
