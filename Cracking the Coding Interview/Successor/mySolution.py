09c5afdcfa4f459267fc2bf163c4dc570646cebe
junbangg <wnstjr4620@naver.com>
2021-01-21 18:26:29 +0900

    Not finished

---
+++ Cracking the Coding Interview/Successor/mySolution.py
@@ -0,0 +1,23 @@
+
+
+def inorder(root, prev, val):
+    if root:
+        inorder(root, prev, val)
+        if val == prev:
+            return root.key
+        else:
+            prev = root.key
+        inorder(root, prev, val)
+
+    if root:
+        if root.left and inorder(root.left, prev, val) == val:
+            return root.val
+        if root.right and inorder(root.right, prev, val) == val:
+            return root.val
+        return root.val
+
+
+def mySolution(root, val):
+    return inorder(root, None, val)
+
+
