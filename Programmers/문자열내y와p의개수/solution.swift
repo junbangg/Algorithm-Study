2f69158e87248ad4920ccf5a62b33c8b6724aae5
junbangg <wnstjr4620@naver.com>
2021-01-25 21:43:27 +0900

    changed swift solution

--- Programmers/문자열내y와p의개수/solution.swift
+++ Programmers/문자열내y와p의개수/solution.swift
@@ -2,7 +2,5 @@ import Foundation
 
 func solution(_ s:String) -> Bool
 {
-    let p = s.lowercased().filter({$0 == "p"}).count
-    let y = s.lowercased().filter({$0 == "y"}).count
-    return p == y
+    return s.lowercased().filter({$0 == "p"}).count == s.lowercased().filter({$0 == "y"}).count
 }
