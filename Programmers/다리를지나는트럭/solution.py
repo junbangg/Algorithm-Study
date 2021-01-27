480c02c6547ef7afda75fb961bf9011bd8d811ee
junbangg <wnstjr4620@naver.com>
2021-01-27 18:56:32 +0900

    다리를 지나는 트럭

---
+++ Programmers/다리를지나는트럭/solution.py
@@ -0,0 +1,23 @@
+def solution(bridge_length, weight, truck_weights):
+    time = 1
+    cap = 0
+    t_out = []
+    t_in = [(truck,0) for truck in truck_weights]
+    bridge = []
+    while truck_weights != t_out:
+        if t_in and cap + list(t_in[0])[0] <= weight:
+            truck = t_in.pop(0)
+            bridge.append(truck)
+            key,val = truck
+            cap += key
+        time += 1
+        for ind, tup in enumerate(bridge):
+            tup = list(tup)
+            tup[1] += 1
+            bridge[ind] = tuple(tup)
+        key, val = bridge[0]
+        if val >= bridge_length:
+            bridge.pop(0)
+            cap -= key
+            t_out.append(key)
+    return time
