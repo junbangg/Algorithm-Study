52885ecdab4f389fa5f56b5af5a56f8d01c97071
junbangg <wnstjr4620@naver.com>
2021-01-29 15:44:27 +0900

    added to HOF

---
+++ CODE Hall of Fame/Programmers-큰수만들기/solution.py
@@ -0,0 +1,9 @@
+def solution(number, k):
+    s = []
+
+    for i in range(len(number)):
+        while s and k > 0 and s[-1] < number[i]:
+            s.pop()
+            k -= 1
+        s.append(number[i])
+    return ''.join(s[:len(number) - k])
