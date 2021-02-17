dc4f63930ffce550a093806c233d3b7164f00c6f
junbangg <wnstjr4620@naver.com>
2021-02-17 23:44:59 +0900

    fixed

--- Programmers/조이스틱/solution.py
+++ Programmers/조이스틱/solution.py
@@ -38,3 +38,43 @@ def solution(name):
             answer += fwd
             ind.remove(cur)
     return answer
+
+# Answer
+def solution(name):
+    codes = list(map(ord, name))
+    i, answer, fwd, back = 0, 0, 0, 0
+    end_flag = False
+    l_name = len(name)
+    # A 아닌 index 로 list 생성
+    ind = list(i for i, j in enumerate(list(name)) if j != 'A')
+    while ind:
+        cur = ind[i]
+        letter = codes[cur]
+        if letter <= 78: answer += letter - 65
+        else: answer += 91 - letter
+        if len(ind) == 1: fwd, back = 0, 0
+        else:
+        # i 뒤 타겟과의 거리
+            if i == 0:
+                back = cur + 1
+                end_flag = True
+            else:
+                back = cur - ind[i-1]
+            # i 앞 타겟 과의 거리
+            if i == len(ind) - 1:
+                fwd = ind[0] + 1
+            if i < len(ind) - 1:
+                fwd = ind[i+1] - cur
+        # 비교
+        if back < fwd or cur == l_name - 1:
+            answer += back
+            ind.remove(cur)
+            if end_flag:
+                i = len(ind) - 1
+                end_flag = False
+            else:
+                i -= 1
+        else:
+            answer += fwd
+            ind.remove(cur)
+    return answer
