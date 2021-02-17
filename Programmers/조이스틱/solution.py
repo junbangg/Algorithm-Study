d6de538203ab35cb1a211395d534c507a4775f39
junbangg <wnstjr4620@naver.com>
2021-02-17 22:19:37 +0900

    조이스틱

---
+++ Programmers/조이스틱/solution.py
@@ -0,0 +1,40 @@
+# first try
+def solution(name):
+    codes = list(map(ord, name))
+    # Change to list of indexes that are not 'A'
+    ind = list(i for i, j in enumerate(list(name)) if j != 'A')
+    # variables
+    i, answer, fwd, back = 0, 0, 0, 0
+    end_flag = False
+    l_name = len(name)
+    while ind:
+        cur = ind[i]
+        letter = codes[cur]
+        if letter <= 78: answer += letter - 65
+        else: answer += 91 - letter
+        if len(ind) == 1: fwd, back = 0, 0
+        else:
+        # i 뒤 타겟과의 거리
+            if i == 0:
+                back = l_name - ind[-1]
+                end_flag = True
+            else:
+                back = ind[i-1]
+            # i 앞 타겟 과의 거리
+            if i == len(ind) - 1:
+                fwd = ind[0] + 1
+            if i < len(ind) - 1:
+                fwd = ind[i+1] - cur
+        # 비교
+        if back < fwd:
+            answer += back
+            ind.remove(cur)
+            if end_flag:
+                i = len(ind) - 1
+                end_flag = False
+            else:
+                i -= 1
+        else:
+            answer += fwd
+            ind.remove(cur)
+    return answer
