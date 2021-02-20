6d26860d163881b2fa1e22369bd9b049e5963d5b
junbangg <wnstjr4620@naver.com>
2021-02-20 22:25:24 +0900

    fixed answer

--- Leet Code/Group Anagrams/solution.py
+++ Leet Code/Group Anagrams/solution.py
@@ -1,13 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        results = []
-        while strs:
-            word = strs.pop()
-            anagrams = [word]
-            while strs:
-                check = strs[len(strs)-1]
-                if Counter(word) == Counter(check):
-                    anagrams.append(check)
-                    strs.pop()
-            results.append(anagrams)
-        return results
+        dic = {}
+        for i, v in enumerate([''.join(sorted(c)) for c in strs]):
+            if v not in dic:
+                dic[v] = [strs[i]]
+            else: dic[v].append(strs[i])
+        return dic.values()
