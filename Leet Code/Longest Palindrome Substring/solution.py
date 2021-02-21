8cf83e9247acbdd1e1e7cd5c6fae542dd303b814
junbangg <wnstjr4620@naver.com>
2021-02-21 15:06:25 +0900

    added optimized

--- Leet Code/Longest Palindrome Substring/solution.py
+++ Leet Code/Longest Palindrome Substring/solution.py
@@ -7,3 +7,16 @@ class Solution:
                 if substring == substring[::-1] and len(substring) > len(answer):
                     answer = substring
         return answer
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2 or s == s[::-1]: return s
+        def expand(left, right):
+            while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+                left -= 1
+                right += 1
+            return s[left + 1:right - 1]
+
+        longest = ""
+        for i in range(len(s) - 1):
+            longest = max(longest, expand(i, i+1), expand(i, i+2), key = len)
+        return longest
