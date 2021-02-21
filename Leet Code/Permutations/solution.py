77eaf6926322eefbdbba7ac1c55bdcb13f24c44e
junbangg <wnstjr4620@naver.com>
2021-02-21 17:25:44 +0900

    Without-library version

--- Leet Code/Permutations/solution.py
+++ Leet Code/Permutations/solution.py
@@ -1,3 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         return itertools.permutations(nums, len(nums))
+
+# or
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+        prev_elements = []
+        def dfs(elements):
+            if len(elements) == 0:
+                answer.append(prev_elements[:])
+            for i in elements:
+                temp = elements[:]
+                temp.remove(i)
+
+                prev_elements.append(i)
+                dfs(temp)
+                prev_elements.pop()
+        dfs(nums)
+        return answer
