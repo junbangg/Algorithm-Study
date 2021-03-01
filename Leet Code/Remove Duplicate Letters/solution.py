245687b4d0176f6a40aadf882acd730a6ddaf39e
junbangg <wnstjr4620@naver.com>
2021-03-01 15:30:13 +0900

    Solution using stacks

--- Leet Code/Remove Duplicate Letters/solution.py
+++ Leet Code/Remove Duplicate Letters/solution.py
@@ -5,3 +5,15 @@ class Solution:
             if set(s) == set(suffix):
                 return char + self.removeDuplicateLetters(suffix.replace(char, ''))
         return ''
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack, counter = [], collections.Counter(s)
+        for char in s:
+            counter[char] -= 1
+            if char in stack:
+                continue
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                stack.pop()
+            stack.append(char)
+        return ''.join(stack)
