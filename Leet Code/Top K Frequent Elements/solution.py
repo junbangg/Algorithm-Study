beefd46fcf1a487a56eb54a5d4fff4a5b77ab03d
junbangg <wnstjr4620@naver.com>
2021-03-07 21:36:57 +0900

    added another solution

--- Leet Code/Top K Frequent Elements/solution.py
+++ Leet Code/Top K Frequent Elements/solution.py
@@ -1,4 +1,20 @@
+# My first attempt
 def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = collections.Counter(nums).most_common()
         return [i for i, v in counter][:k]
+
+
+
+# Using Counter and MinHeap
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = collections.Counter(nums)
+        freqs_heap = []
+        for f in counter:
+            heapq.heappush(freqs_heap, (-counter[f], f))
+        topq = list()
+        for _ in range(k):
+            topq.append(heapq.heappop(freqs_heap)[1])
+        return topq
+
+# Pythonic Way
 
