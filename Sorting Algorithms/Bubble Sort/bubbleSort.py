2165c149caee290588fa463a3b8998774b666804
junbangg <wnstjr4620@naver.com>
2021-03-12 12:15:08 +0900

    fixed bubblesort

--- Sorting Algorithms/Bubble Sort/bubbleSort.py
+++ Sorting Algorithms/Bubble Sort/bubbleSort.py
@@ -2,9 +2,7 @@ def bubbleSort(nums):
     for i in range(len(nums)):
         for j in range(len(nums)-1):
             if nums[i] < nums[j]:
-                temp = nums[i]
-                nums[i] = nums[j]
-                nums[j] = temp
+                nums[j], nums[j+1] = nums[j+1], nums[j]
     return nums
 
 nums = [55,2,3,4,23,56,23,4,33]
