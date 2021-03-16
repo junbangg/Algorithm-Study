e445372f45a79798fea166b6a58de677702b3b07
junbangg <wnstjr4620@naver.com>
2021-03-17 00:23:52 +0900

    Naive Approach

---
+++ Leet Code/Insertion Sort List/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# First Attempt (Naive)
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        L = []
+        p = head
+        while p:
+            L.append(p.val)
+            p = p.next
+        for i in range(1, len(L)):
+            for j in range(i, 0, -1):
+                if L[j-1] > L[j]:
+                    L[j], L[j-1] = L[j-1], L[j]
+        p = head
+        for i in range(len(L)):
+            p.val = L[i]
+            p = p.next
+        return head
+
+# Second Attempt
