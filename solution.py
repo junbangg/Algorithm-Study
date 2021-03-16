2af34b4203e71bcbd2cdf4ba42ca1bde0872ad67
junbangg <wnstjr4620@naver.com>
2021-03-16 17:06:19 +0900

    first attempt

---
+++ solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x: x[0])
+        answers = []
+        #edge case
+        if len(intervals) == 1:
+            return intervals
+        # reverse stack
+        intervals = intervals[::-1]
+        l1 = intervals.pop()
+        i = 0
+        # merge
+        while intervals:
+            l2 = intervals.pop()
+            if l1[1] >= l2[0]:
+                l3 = [min(l1[0], l2[0]), max(l1[1], l2[1])]
+                if answers:
+                    answers.pop()
+                answers.append(l3)
+                l1 = l3
+            else:
+                if i == 0:
+                    answers.append(l1)
+                answers.append(l2)
+                l1 = l2
+            i += 1
+        return answers
