64e927f6a965b7b49dbc5d210c4653c76ad46a44
junbangg <wnstjr4620@naver.com>
2021-03-16 18:04:34 +0900

    added Book Solution(more concise)

--- Leet Code/Merge Intervals/solution.py
+++ Leet Code/Merge Intervals/solution.py
@@ -45,3 +45,30 @@ class Solution:
                 intervals[p1] = merged
             p2 -= 1
         return intervals[::-1]
+
+# Best Solution
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # edge case 
+        if len(intervals) == 1:
+            return intervals
+        merged = []
+        for i in sorted(intervals, key = lambda x : x[0]):
+            if merged and i[0] <= merged[-1][1]:
+                merged[-1][1] = max(i[1], merged[-1][1])
+            else:
+                merged += i,
+        return merged
+
+
+
+
+
+
+
+
+
+
+
+
+
