50ef61e43cbcf317c4b7bec3d3d157eda117e3e2
junbangg <wnstjr4620@naver.com>
2021-03-16 15:42:12 +0900

    Merge Sort version solution

--- Leet Code/Sort List/solution.py
+++ Leet Code/Sort List/solution.py
@@ -16,3 +16,27 @@ class Solution:
             p.val = arr[i]
             p = p.next
         return head
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
+
+    def sortList(self, head: ListNode) -> ListNode:
+        # edge case
+        if not (head and head.next):
+            return head
+        # Runner 기법
+        mid, slow, fast = None, head, head
+        while fast and fast.next:
+            mid, slow, fast = slow, slow.next, fast.next.next
+        mid.next = None
+        # 분할 재귀 호출 
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+        # 합병
+        return self.mergeTwoLists(l1, l2)
+
