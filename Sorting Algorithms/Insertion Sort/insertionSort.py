fc9d08aa348126dc439edb64fd974fe134e1cbd0
junbangg <wnstjr4620@naver.com>
2021-03-16 23:54:08 +0900

    Insertion Sort implementation

---
+++ Sorting Algorithms/Insertion Sort/insertionSort.py
@@ -0,0 +1,9 @@
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j-1] > arr[j]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+    return arr
+
+test = [6,5,4,3,2,1]
+print(insertionSort(test))
