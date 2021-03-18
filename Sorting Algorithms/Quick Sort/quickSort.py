776831d1cc7e850b1efeb72bbcd8b6abd98170e3
junbangg <wnstjr4620@naver.com>
2021-03-18 17:43:58 +0900

    added Hoare's version of Quick Sort

--- Sorting Algorithms/Quick Sort/quickSort.py
+++ Sorting Algorithms/Quick Sort/quickSort.py
@@ -1,4 +1,29 @@
-def quickSort(nums, low, high):
+# Hoare 
+def quickSort_hoare(nums, low, high):
+    def partition(nums, pivot, high):
+        left = pivot + 1
+        right = high
+        while True:
+            while left < high and nums[left] < nums[pivot]:
+                left += 1
+            while right > pivot and nums[right] > nums[pivot]:
+                right -= 1
+            if right <= left:
+                break
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        nums[pivot], nums[right] = nums[right], nums[pivot]
+        return right
+
+    if low < high:
+        pivot = partition(nums, low, high)
+        quickSort_hoare(nums, low, pivot - 1)
+        quickSort_hoare(nums, pivot + 1, high)
+    return nums
+
+# Lomuto 
+def quickSort_lomuto(nums, low, high):
     def partition(low, high):
         pivot = nums[high]
         left = low
@@ -11,10 +36,10 @@ def quickSort(nums, low, high):
 
     if low < high:
         pivot = partition(low, high)
-        quickSort(nums, low, pivot - 1)
-        quickSort(nums, pivot + 1, high)
+        quickSort_lomuto(nums, low, pivot - 1)
+        quickSort_lomuto(nums, pivot + 1, high)
 
     return nums
 
 nums = [4,3,2,2,3,1]
-print(quickSort(nums, 0, len(nums)-1))
+print(quickSort_hoare(nums, 0, len(nums)-1))
