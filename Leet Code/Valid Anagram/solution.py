2501f60bab3886b5d8480520c05297662d631f88
junbangg <wnstjr4620@naver.com>
2021-03-18 15:55:04 +0900

    added edge case to first solution

--- Leet Code/Valid Anagram/solution.py
+++ Leet Code/Valid Anagram/solution.py
@@ -1,3 +1,10 @@
+# first attempt solution
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+# edge case for first attempt solution
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
         return sorted(s) == sorted(t)
