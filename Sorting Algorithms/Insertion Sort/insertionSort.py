36be12e6f6d2ea1464bbe9c072aec56e18913419
junbangg <wnstjr4620@naver.com>
2021-03-18 15:13:57 +0900

    Added while loop version

--- Sorting Algorithms/Insertion Sort/insertionSort.py
+++ Sorting Algorithms/Insertion Sort/insertionSort.py
@@ -1,9 +1,19 @@
-def insertionSort(arr):
+def insertionSortForVersion(arr):
     for i in range(1, len(arr)):
         for j in range(i, 0, -1):
             if arr[j-1] > arr[j]:
                 arr[j], arr[j-1] = arr[j-1], arr[j]
     return arr
 
+def insertionSortWhileVersion(arr):
+    i = 1
+    while i < len(arr):
+        j = i
+        while j > 0 and arr[j-1] > arr[j]:
+            arr[j-1], arr[j] = arr[j], arr[j-1]
+            j -= 1
+        i += 1
+    return arr
+
 test = [6,5,4,3,2,1]
-print(insertionSort(test))
+print(insertionSortWhileVersion(test))
