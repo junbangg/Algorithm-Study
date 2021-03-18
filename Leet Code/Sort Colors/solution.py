17f371840cbbc848db08d099ed18422365e9471a
junbangg <wnstjr4620@naver.com>
2021-03-18 23:41:39 +0900

    added Dijkstras dutch national flag solution

--- Leet Code/Sort Colors/solution.py
+++ Leet Code/Sort Colors/solution.py
@@ -1,3 +1,20 @@
+# Obvious Solution
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         nums.sort()
+
+# # Dijkstras Dutch National Flag Problem Solution
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        i, j ,k = 0, 0, len(nums)
+        while j < k:
+            if nums[j] < 1:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+            elif nums[j] > 1:
+                k -= 1
+                nums[j], nums[k] = nums[k], nums[j]
+            else:
+                j += 1
+        return nums
