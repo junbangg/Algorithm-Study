c35b2fde0461f2de4680cce63931106a4bdcf00e
junbangg <wnstjr4620@naver.com>
2021-03-19 13:02:30 +0900

    added Dictionary+Enumerate() solution

--- Leet Code/K CLosest Points to Origin/solution.py
+++ Leet Code/K CLosest Points to Origin/solution.py
@@ -1,16 +1,3 @@
-class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def distance(x, y):
-            return sqrt(pow(x, 2) + pow(y, 2))
-        ogPoints = collections.defaultdict(list)
-        dist = [distance(p[0], p[1]) for p in points]
-        for i, d in enumerate(dist):
-            ogPoints[d].append(points[i])
-        dist.sort()
-        print(ogPoints)
-        print(dist)
-        return [point for point in ogPoints[d] for d in dist[:k]]
-
 # insertion sort approach (too slow)
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
@@ -25,3 +12,19 @@ class Solution:
                 j -= 1
             i += 1
         return points[:k]
+
+# Dictionary Approach
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def distance(x, y):
+            return sqrt(pow(x, 2) + pow(y, 2))
+        ogPoints = collections.defaultdict(list)
+        dist = [distance(p[0], p[1]) for p in points]
+        for i, d in enumerate(dist):
+            ogPoints[d].append(points[i])
+        dist.sort()
+        print(ogPoints)
+        print(dist)
+        return [point for point in ogPoints[d] for d in dist[:k]]
+
+
