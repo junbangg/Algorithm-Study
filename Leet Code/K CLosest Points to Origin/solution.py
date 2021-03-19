dbe90187e480dff8f08922ed309d38a65e4a19f6
junbangg <wnstjr4620@naver.com>
2021-03-19 15:51:52 +0900

    trimmed down minheap solution

--- Leet Code/K CLosest Points to Origin/solution.py
+++ Leet Code/K CLosest Points to Origin/solution.py
@@ -54,8 +54,7 @@ class Solution:
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
-        for p in points:
-            x, y = p[0], p[1]
+        for (x,y) in points:
             distance = sqrt(x**2 + y**2)
             heapq.heappush(heap, (distance, [x, y]))
         return [heapq.heappop(heap)[1] for _ in range(k)]
