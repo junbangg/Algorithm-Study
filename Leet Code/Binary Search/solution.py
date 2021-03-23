f5273919d4ca8b46b4556a260ea822978e3f5245
junbangg <wnstjr4620@naver.com>
2021-03-23 21:06:51 +0900

    Added iterative solution

--- Leet Code/Binary Search/solution.py
+++ Leet Code/Binary Search/solution.py
@@ -1,18 +1,30 @@
+# Recursive Solution
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # edge case
-        if target not in nums:
-            return -1
-        def binarySearch(arr, low, high):
+        def binarySearch(low, high):
             mid = (low + high) // 2
             # base case
             if high >= low:
-                if arr[mid] == target:
+                if nums[mid] == target:
                     return mid
-                elif arr[mid] > target:
-                    return binarySearch(arr, low, mid - 1)
+                elif nums[mid] > target:
+                    return binarySearch(low, mid - 1)
                 else:
-                    return binarySearch(arr, mid + 1, high)
+                    return binarySearch(mid + 1, high)
             else:
                 return -1
-        return binarySearch(nums, 0, len(nums))
+        return binarySearch(0, len(nums)-1)
+
+# Iterative Solution
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
