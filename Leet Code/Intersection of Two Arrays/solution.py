bd1347ad9d35419955f3ef53134afcadcaf0171b
junbangg <wnstjr4620@naver.com>
2021-03-24 14:09:00 +0900

    Two Pointer solution

--- Leet Code/Intersection of Two Arrays/solution.py
+++ Leet Code/Intersection of Two Arrays/solution.py
@@ -30,3 +30,21 @@ class Solution:
             if len(nums2) > 0 and index < len(nums2) and i == nums2[index]:
                 answer.add(nums2[index])
         return answer
+
+# Two Pointer Solution
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        answer = set()
+        nums1.sort()
+        nums2.sort()
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]:
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                answer.add(nums1[i])
+                i += 1
+                j += 1
+        return answer
