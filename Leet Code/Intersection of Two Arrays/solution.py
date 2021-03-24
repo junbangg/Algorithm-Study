cf43739c2d62e6d7c2f8a6ad40c79b763b65bfdc
junbangg <wnstjr4620@naver.com>
2021-03-24 13:30:29 +0900

    Trimmed down solution

--- Leet Code/Intersection of Two Arrays/solution.py
+++ Leet Code/Intersection of Two Arrays/solution.py
@@ -5,18 +5,17 @@ def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
 # Without using the comforts of Python
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        answer = []
+        answer = set()
         nums2.sort()
         for i in nums1:
             left, right = 0, len(nums2) - 1
             while left <= right:
                 mid = left + (right - left) // 2
                 if nums2[mid] == i:
-                    answer.append(i)
+                    answer.add(i)
                     break
                 elif nums2[mid] > i:
                     right = mid - 1
                 else:
                     left = mid + 1
-        return list(set(answer))
-
+        return answer 
