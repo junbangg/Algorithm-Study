653527807e9085473933525b51ee48b0c973c3bf
junbangg <wnstjr4620@naver.com>
2021-03-24 13:22:45 +0900

    added Binary Search solution

--- Leet Code/Intersection of Two Arrays/solution.py
+++ Leet Code/Intersection of Two Arrays/solution.py
@@ -3,4 +3,20 @@ def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2))
 
 # Without using the comforts of Python
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        answer = []
+        nums2.sort()
+        for i in nums1:
+            left, right = 0, len(nums2) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums2[mid] == i:
+                    answer.append(i)
+                    break
+                elif nums2[mid] > i:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return list(set(answer))
 
