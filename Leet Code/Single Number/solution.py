8693294f88f94f12869a51e8e1e306ffbfea9357
junbangg <wnstjr4620@naver.com>
2021-03-25 15:39:00 +0900

    added bit manipulation solution

--- Leet Code/Single Number/solution.py
+++ Leet Code/Single Number/solution.py
@@ -3,3 +3,12 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         count = Counter(nums)
         return min(count, key = count.get)
+
+# Bit Manipulation
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single = 0
+        for i in nums:
+            single ^= i
+        return single
+
