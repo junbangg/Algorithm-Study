234a9747f0321a44b9475207cfa7ed78ad9c5a28
junbangg <wnstjr4620@naver.com>
2021-03-26 21:29:19 +0900

    added solution

--- Leet Code/Number of 1-bits/solution.py
+++ Leet Code/Number of 1-bits/solution.py
@@ -1,3 +1,12 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         return bin(n).count('1')
+
+# Bit Manipulation Solution
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            n &= n - 1
+            count += 1
+        return count
