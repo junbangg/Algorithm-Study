9441b6bc7771a07dc4949240cc68b7c25fa043f1
junbangg <wnstjr4620@naver.com>
2021-03-26 14:09:20 +0900

    Added new solution

--- Leet Code/Reverse Bits/solution.py
+++ Leet Code/Reverse Bits/solution.py
@@ -1,3 +1,4 @@
+# Difficult Solution
 class Solution:
     def reverseBits(self, n):
         n = (n >> 16) | (n << 16)
@@ -6,3 +7,20 @@ class Solution:
         n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
         n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
         return n
+
+# Intuitive Solution
+class Solution:
+    def reverseBits(self, n):
+        # Save reversed Bits to list
+        bitList = []
+        for _ in range(32):
+            bitList.append(n&1)
+            n = n >> 1
+        # Reverse List
+        bitList = bitList[::-1]
+        # pop() bits to create new integer
+        result = 0
+        while bitList:
+            result = result << 1
+            result += bitList.pop()
+        return result
