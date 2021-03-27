4d041d86c6835512f38e96ab6838676855fe2367
junbangg <wnstjr4620@naver.com>
2021-03-27 21:01:33 +0900

    added best code

--- Leet Code/Sum of Two Integers/solution.py
+++ Leet Code/Sum of Two Integers/solution.py
@@ -132,3 +132,15 @@ class Solution:
             result = ~(result ^ MASK)
         
         return result
+
+# Most concise answer
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF
+        INT_MAX = 0x7FFFFFFF
+        while b != 0:
+            a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
+        
+        if a > INT_MAX:
+            return ~(a ^ MASK)
+        return a
