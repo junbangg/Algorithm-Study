6dbc6322ecb599eadadbfc47d010ed3696eafadc
junbangg <wnstjr4620@naver.com>
2021-03-27 20:48:10 +0900

    added solution

--- Leet Code/Sum of Two Integers/solution.py
+++ Leet Code/Sum of Two Integers/solution.py
@@ -48,3 +48,33 @@ class Solution:
             answer = answer << 1
             answer += result.pop()
         return -abs(answer) if negative else answer 
+
+# Solution
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF
+        INT_MAX = 0x7FFFFFFF
+        a_bits = bin(a & MASK)[2:].zfill(32)
+        b_bits = bin(b & MASK)[2:].zfill(32)
+        result = []
+        sum, carry = 0, 0
+        for i in range(32):
+            A = int(a_bits[31-i])
+            B = int(b_bits[31-i])
+            # Operations
+            Q1 = A & B
+            Q2 = A ^ B
+            Q3 = Q2 & carry
+            sum = carry ^ Q2
+            carry = Q1 | Q3
+            
+            result.append(str(sum))
+        if carry == 1:
+            result.append('1')
+        
+        result = int(''.join(result[::-1]), 2) & MASK
+        
+        if result > INT_MAX:
+            result = ~(result ^ MASK)
+        
+        return result
