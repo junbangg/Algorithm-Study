83b4ec63791511979dfbe9327ef9f0e4fd42f9ed
junbangg <wnstjr4620@naver.com>
2021-03-28 17:46:53 +0900

    Added accepted answer

--- Leet Code/Sliding Window Maximum/solution.py
+++ Leet Code/Sliding Window Maximum/solution.py
@@ -39,3 +39,22 @@ class Solution:
             if windowmax == window.popleft():
                 windowmax = float('inf')
         return answer
+
+# Accepted Answer
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        window = collections.deque()
+        result = []
+        for index, value in enumerate(nums):
+            # check if window is inside sliding window range
+            # (index - k) = sliding window starting index
+            if window and window[0] <= index - k:
+                window.popleft()
+            # Empty window until new value is at the front(if the new value is the max number)
+            while window and nums[window[-1]] <= value:
+                window.pop()
+            window.append(index)
+            # k-1 => sliding window last index
+            if index >= k - 1:
+                result.append(nums[window[0]])
+        return result
