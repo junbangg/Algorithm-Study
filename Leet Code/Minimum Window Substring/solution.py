b7cebe72d7f02807d7dc8a6836ba0f1d20e87a35
junbangg <wnstjr4620@naver.com>
2021-03-29 18:03:18 +0900

    Added accepted solution

--- Leet Code/Minimum Window Substring/solution.py
+++ Leet Code/Minimum Window Substring/solution.py
@@ -35,4 +35,29 @@ class Solution:
         return ''.join(result)
 
 
-
+# Accepted O(n) 풀이
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.Counter(t)
+        missing = len(t)
+        # start, end 가 최소 윈도우 사이즈 기록
+        left = start = end = 0
+        for right, char in enumerate(s, 1):
+            # 필요한거면 1 빼기
+            if need[char] > 0:
+                missing -= 1
+            need[char] -= 1
+            # 다 찾았으면 left 조절해서 윈도우 사이즈 줄이기
+            if missing == 0:
+                # need[s[left]] < 0 이라는 것은 우리가 찾는 단어가 아니라는 뜻
+                # 그래서 left 가 우리가 찾는거 아닐 때마다 left로 윈도우를 좁힌다
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+                # 지금 윈도우가 더 작으면 인덱스 저장 
+                if not end or right - left <= end - start:
+                    start, end = left, right
+                need[s[left]] += 1
+                missing += 1
+                left += 1
+        return s[start:end]
---
+++ Leet Code/Minimum Window Substring/test.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+a = ['a', 'b', 'a', 'c']
+count = Counter(a)
+
+print(count['d'])
