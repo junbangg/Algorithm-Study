16d27399b4a95363d5b4fb28d9b58e5cb8630b59
junbangg <wnstjr4620@naver.com>
2021-03-30 14:24:00 +0900

    added second try solution

--- Leet Code/UTF-8 Validation/solution.py
+++ Leet Code/UTF-8 Validation/solution.py
@@ -19,3 +19,30 @@ class Solution:
             else:
                 return False
         return True
+
+# retried solution
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        def checkCount(nxt, count):
+            tens = 0
+            for i in range(nxt + 1, nxt + count):
+                byte = bin(data[i])[2:].zfill(8)
+                if byte[:2] == '10':
+                    tens += 1
+            if tens == count - 1:
+                return True
+            return False
+        i = 0
+        while i < len(data):
+            byte = bin(data[i])[2:].zfill(8)
+            if byte[:3] == '110' and i + 2 <= len(data) and checkCount(i, 2) :
+                i += 2 
+            elif byte[:4] == '1110' and i + 3 <= len(data) and checkCount(i, 3):
+                i += 3 
+            elif byte[:5] == '11110' and i + 4 <= len(data) and checkCount(i, 4):
+                i += 4 
+            elif byte[0] == '0':
+                i += 1
+            else:
+                return False
+        return True
