bb2e551fef4863d1cc1d8b3ce4ae8286030b850f
junbangg <wnstjr4620@naver.com>
2021-04-02 20:51:15 +0900

    Added Boyer-Moore algorithm

--- Leet Code/Majority Element/solution.py
+++ Leet Code/Majority Element/solution.py
@@ -24,3 +24,20 @@ class Solution:
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         return sorted(nums)[len(nums)//2]
+
+# Hash Map
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key = counts.get)
+
+# Boyer-Moore Voting algorithm
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        return candidate
