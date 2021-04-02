dc55ab34770e6e3839e0bb131cbec6c03ed8fbd6
junbangg <wnstjr4620@naver.com>
2021-04-02 23:03:10 +0900

    Added 2nd attempt code

--- Programmers/주식가격/answer.py
+++ Programmers/주식가격/answer.py
@@ -1,3 +1,4 @@
+# 1st
 def solution(prices):
     answer = []
     for i in range(0, len(prices)):
@@ -8,4 +9,15 @@ def solution(prices):
             if current > prices[j]:
                 break
         answer.append(counter)
+    return answer
+# 2nd
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        cur, change = prices[i], 0
+        for j in range(i + 1, len(prices)):
+            change += 1
+            if prices[j] < cur:
+                break
+        answer[i] = change
     return answer
