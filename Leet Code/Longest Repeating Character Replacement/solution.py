1698ff234937a83106013610598d2037fa058185
junbangg <wnstjr4620@naver.com>
2021-04-02 21:16:33 +0900

    second attempt code

--- Leet Code/Longest Repeating Character Replacement/solution.py
+++ Leet Code/Longest Repeating Character Replacement/solution.py
@@ -1,31 +1,21 @@
+# 2nd Try
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        s = list(s)
-        chances = k
-        left, right, length = 0, 1, 1
-        answer = []
-        prev = s[left]
-        while right < len(s):
-            # 중복 문자일때
-            if prev == s[right]:
-                length += 1
-                right += 1
-            elif chances == 0:
-                answer.append(length)
-                chances = k
-                left += 1
-                right = left + 1
-                length = 1
-                prev = s[left]
+        if not s:
+            return 0
+        left = right = 0
+        start = end = 0
+        count = collections.defaultdict(int)
+        while left < len(s) and right < len(s):
+            count[s[right]] += 1
+            if (right - left + 1) - max(count.values()) <= k:
+                if right - left + 1 > end - start + 1:
+                    start, end = left, right
             else:
-                chances -= 1
-                length += 1
-                right += 1
-        answer.append(length)            
-        return max(answer)
-    # Doesn't work because of ABBB
-    # above code doesn't consider changing A to B
-
+                count[s[left]] -= 1
+                left += 1
+            right += 1
+        return end - start + 1
 # Accepted Code
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
