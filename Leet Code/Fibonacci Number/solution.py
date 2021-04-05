678701321610e15fe04eba89935b872b945e26bb
junbangg <wnstjr4620@naver.com>
2021-04-05 22:10:24 +0900

    Added memoization

--- Leet Code/Fibonacci Number/solution.py
+++ Leet Code/Fibonacci Number/solution.py
@@ -1,3 +1,4 @@
+# Recursion Solution
 class Solution:
     def fib(self, n: int) -> int:
         if n == 1:
@@ -5,3 +6,15 @@ class Solution:
         if n == 0:
             return 0
         return self.fib(n-2) + self.fib(n-1)
+
+# Memoization Solution
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = collections.defaultdict(int)
+        def recurse(n):
+            if n <= 1:
+                return n
+            if dp[n] == 0:
+                dp[n] = recurse(n - 2) + recurse(n - 1)
+            return dp[n]
+        return recurse(n)
