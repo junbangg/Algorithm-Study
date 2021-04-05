3a398f8094a6771ba3e098fceef95df4d132fd1c
junbangg <wnstjr4620@naver.com>
2021-04-05 18:40:45 +0900

    added Binary Search solution

--- Programmers/입국심사/solution.py
+++ Programmers/입국심사/solution.py
@@ -29,3 +29,21 @@ def solution(n, times):
         print(h)
         print(answer)
     return answer
+
+# Binary Search
+def solution(n, times):
+    left, right = 1, max(times) * n
+    answer = 0
+    while left <= right:
+        mid = left + (right - left) // 2
+        people = 0
+        for t in times:
+            people += mid // t
+            if people >= n:
+                break
+        if people >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return answer
