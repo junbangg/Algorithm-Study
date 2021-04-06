d8873a94eac6fabc4016273f75b34e9fc90d05c3
junbangg <wnstjr4620@naver.com>
2021-04-06 22:26:17 +0900

    Added knapsack problem

---
+++ Other/knapsack.py
@@ -0,0 +1,28 @@
+# 1-0 Knapsack Problem
+cargo = [(4, 12),
+         (2, 1),
+         (10, 4),
+         (1,1),
+         (2,2) ]
+capacity = 15
+
+def knapsack(cargo, capacity):
+    sack = []
+    for i in range(len(cargo) + 1):
+        sack.append([])
+        for j in range(capacity + 1):
+            if i == 0 or j == 0:
+                sack[i].append(0)
+            elif cargo[i-1][1] <= j:
+                # add maximum value
+                sack[i].append(
+                    max(cargo[i-1][0] + sack[i-1][j - cargo[i-1][1]],
+                        sack[i-1][j])
+                )
+                pass
+            else:
+                sack[i].append(sack[i-1][j])
+    return sack[-1][-1]
+
+a = knapsack(cargo, capacity)
+print(a)
