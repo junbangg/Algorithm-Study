59fb2bcd2c69386b21434422f507ea1f63f34e32
junbangg <wnstjr4620@naver.com>
2021-04-06 22:50:00 +0900

    fixed code

--- Other/knapsack.py
+++ Other/knapsack.py
@@ -13,13 +13,10 @@ def knapsack(cargo, capacity):
         for j in range(capacity + 1):
             if i == 0 or j == 0:
                 sack[i].append(0)
-            elif cargo[i-1][1] <= j:
-                # add maximum value
-                sack[i].append(
-                    max(cargo[i-1][0] + sack[i-1][j - cargo[i-1][1]],
-                        sack[i-1][j])
-                )
-                pass
+            elif cargo[i - 1][1] <= j:
+                # max value 
+                sack[i].append(max(cargo[i-1][0] + sack[i-1][j - cargo[i-1][1]],
+                                   sack[i-1][j]))
             else:
                 sack[i].append(sack[i-1][j])
     return sack[-1][-1]
