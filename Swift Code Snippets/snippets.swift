9187adebcd1c806d68f01ad9da270dc6a7f4076a
junbangg <wnstjr4620@naver.com>
2021-04-07 16:12:58 +0900

    Added indexing code

--- Swift Code Snippets/snippets.swift
+++ Swift Code Snippets/snippets.swift
@@ -3,5 +3,12 @@ let N = String(N, radix: 2)
 // Binary String ->  Integer
 let N = Int(strtoul("1111000", nil, 2))
 
+// Array indexing
+//Insert
+A.insert("somthing", at: 0)
+
+//Pop()
+A.removeLast()
+
 
 
