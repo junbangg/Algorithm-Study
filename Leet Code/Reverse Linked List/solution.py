6c900e4592c5c765abe845cff51a47acd7be06d7
junbangg <wnstjr4620@naver.com>
2021-04-12 16:06:40 +0900

    Added solution

--- Leet Code/Reverse Linked List/solution.py
+++ Leet Code/Reverse Linked List/solution.py
@@ -4,3 +4,12 @@ class Solution:
         while cur:
             rev, rev.next, cur= cur, rev, cur.next
         return rev
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        def reverse(node, prev = None):
+            if not node:
+                return prev
+            next, node.next = node.next, prev
+            return reverse(next, node)
+        return reverse(head)
