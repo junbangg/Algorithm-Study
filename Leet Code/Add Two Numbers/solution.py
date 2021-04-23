0ed0ad488645eb989f27f47876e73a86b4f6cb34
junbangg <wnstjr4620@naver.com>
2021-04-23 17:29:41 +0900

    added full adder solution

--- Leet Code/Add Two Numbers/solution.py
+++ Leet Code/Add Two Numbers/solution.py
@@ -18,3 +18,20 @@ class Solution:
             ptr.next = temp
             ptr = ptr.next
         return result
+# full adder approach 
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        root = head = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+        return root.next
