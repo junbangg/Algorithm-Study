2cf9d5e6cdd28c4085902645fefb855e6ddd0198
junbangg <wnstjr4620@naver.com>
2021-04-24 22:58:19 +0900

    Added solution

--- Leet Code/Combinations/solution.py
+++ Leet Code/Combinations/solution.py
@@ -27,3 +27,17 @@ class Solution:
                 elements.pop()
         recurse([], 1)
         return results
+
+# or
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        results = []
+        def dfs(index, path):
+            if len(path) == k:
+                results.append(path)
+                return
+            for i in range(index, n + 1):
+                dfs(i+1, path+[i])
+        dfs(1, [])
+        return results
+
