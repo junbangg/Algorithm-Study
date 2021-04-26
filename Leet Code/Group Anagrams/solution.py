7d6ec077596f2d6e6691cd17b97716c6eb7d79ce
junbangg <wnstjr4620@naver.com>
2021-04-27 00:20:41 +0900

    Added horrible solution

--- Leet Code/Group Anagrams/solution.py
+++ Leet Code/Group Anagrams/solution.py
@@ -16,3 +16,27 @@ class Solution:
         for word in strs:
             dic[''.join(sorted(word))].append(word)
         return dic.values()
+
+
+# or
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #deep copy list
+        str_copy = strs[:]
+        #sort each element of list
+        for i in range(len(str_copy)):
+            temp = sorted(str_copy[i])
+            str_copy[i] = ''.join(temp)
+        #create default dict for indices
+        dic = collections.defaultdict(list)
+        for i, v in enumerate(str_copy):
+            dic[v].append(i)
+        # use indices to get original strings
+        result = []
+        for ind in dic.values():
+            temp = []
+            for i in ind:
+                temp.append(strs[i])
+            result.append(temp)
+        return result
+
