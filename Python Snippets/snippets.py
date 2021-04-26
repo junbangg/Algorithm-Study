b236507fe3a51607cd78a914e60daebe58d61bf9
junbangg <wnstjr4620@naver.com>
2021-04-26 23:24:19 +0900

    Added snippet

--- Python Snippets/snippets.py
+++ Python Snippets/snippets.py
@@ -1,7 +1,13 @@
-# get key with maximum value in dictionary
-# https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+'''
+get key with maximum value in dictionary
+https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+'''
+max(dict, key = dict.get)
 
-# remove punctuation from string
-string = re.sub(r'[^/w]', ' ', originalString)
+'''
+remove punctuation from string
+note that it is forward slash \  not /
+'''
+string = re.sub(r'[^\w]', ' ', originalString)
 
 
