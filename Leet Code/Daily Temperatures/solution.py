14f3735b2f7fee88bc8b75447d2e5afc9cfb16f4
junbangg <wnstjr4620@naver.com>
2021-04-28 21:27:17 +0900

    Added new solution

--- Leet Code/Daily Temperatures/solution.py
+++ Leet Code/Daily Temperatures/solution.py
@@ -6,3 +6,18 @@ def dailyTemperatures(self, T: List[int]) -> List[int]:
                 answer[old_index] = index - old_index
             stack.append(index)
         return answer
+#or
+def dailyTemperatures(self, T: List[int]) -> List[int]:
+        dic = collections.defaultdict(collections.deque)
+        stack, answer = [], [0] * len(T)
+        for i, v in enumerate(T):
+            dic[v].append(i)
+        T = T[::-1]
+        while T:
+            temp = T.pop()
+            index = dic[temp].popleft()
+            while stack and stack[-1][0] < temp:
+                startIndex = stack.pop()[1]
+                answer[startIndex] = index - startIndex
+            stack.append([temp, index])
+        return answer
