12a906dd81d3410e2ad0453c62201a71dbc247fd
junbangg <wnstjr4620@naver.com>
2021-04-28 21:32:52 +0900

    Changed solutions

--- Leet Code/Daily Temperatures/solution.py
+++ Leet Code/Daily Temperatures/solution.py
@@ -1,12 +1,4 @@
-def dailyTemperatures(self, T: List[int]) -> List[int]:
-        stack, answer = [], [0] * len(T)
-        for index, temp in enumerate(T):
-            while stack and temp > T[stack[-1]]:
-                old_index = stack.pop()
-                answer[old_index] = index - old_index
-            stack.append(index)
-        return answer
-#or
+# first attempt
 def dailyTemperatures(self, T: List[int]) -> List[int]:
         dic = collections.defaultdict(collections.deque)
         stack, answer = [], [0] * len(T)
@@ -21,3 +13,13 @@ def dailyTemperatures(self, T: List[int]) -> List[int]:
                 answer[startIndex] = index - startIndex
             stack.append([temp, index])
         return answer
+# trimmed down solution
+def dailyTemperatures(self, T: List[int]) -> List[int]:
+        stack, answer = [], [0] * len(T)
+        for index, temp in enumerate(T):
+            while stack and temp > T[stack[-1]]:
+                start = stack.pop()
+                answer[start] = index -start 
+            stack.append(index)
+        return answer
+
