c2b82fa51b0e147ae2d6f17a110c45ee84c239ef
junbangg <wnstjr4620@naver.com>
2021-05-08 01:12:47 +0900

    Added correct solution

--- Programmers/2020카카오인턴십/보석 쇼핑/solution.py
+++ Programmers/2020카카오인턴십/보석 쇼핑/solution.py
@@ -1,27 +1,42 @@
-import sys
-import collections
+import sys, collections
 def solution(gems):
+    # dic for required gems
+    dic = {}
+    for gem in set(gems):
+        dic[gem] = 0
     # length of unique gems(required)
     needs = len(set(gems))
-    # save minimum size window
+    contains = 0
+    # save minimum size window [window size, [startIndex, endIndex]]
     answer = [sys.maxsize, [0,0]]
     # pointers
+    dic[gems[0]] += 1
+    contains += 1
     left, right = 0, 0
     while right < len(gems) and left <= right:
-        # check if current window contains all required gems
-        cur = gems[left:right+1]
-        contains = len(collections.Counter(set(cur)))
-        #if gem is missing
-        if needs != contains:
-            right += 1
-        #if all gems are in window
-        else:
-            # update answer to minimum sized window
-            if len(cur) < answer[0]:
-                answer = [len(cur), [left+1, right+1]]
+        # if current window contains all required gems
+        if contains == needs:
+            # update minimum window
+            if right-left+1 < answer[0]:
+                answer = [right-left+1, [left+1, right+1]]
+            # decrement gem from dic
+            dic[gems[left]] -= 1
+            #if amount of decremented gem is 0..decrement from contain 
+            if dic[gems[left]] == 0:
+                contains -= 1
+            # move pointer
             left += 1
-    return answer[1]
-
+        #if gem is missing
+        else:
+            # move pointer
+            right += 1
+            # check for index out of reach
+            if right < len(gems):
+                # if next gem is a new update to dic..increment to contain
+                if dic[gems[right]] == 0:
+                    contains += 1
+                dic[gems[right]] += 1
+    return answer[1] 
 
 
 gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
