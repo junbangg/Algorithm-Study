1ff17022b1ddde979a4e5de2f066f6d527509e95
junbangg <wnstjr4620@naver.com>
2021-05-07 22:46:32 +0900

    Added new solution

--- Programmers/가장먼노드/solution.py
+++ Programmers/가장먼노드/solution.py
@@ -1,21 +1,33 @@
-from collections import defaultdict
+import collections
 def solution(n, edge):
-    graph = defaultdict(list)
+    # graph
+    graph = collections.defaultdict(list)
     for a, b in edge:
-        graph[a-1].append(b-1)
-    
-    def dfs(v):
-        visited[v] = True
-	for i in graph[v]:
-            if not visited[i]:
-                dfs(i)
-        answer.append(v)
-    
+        graph[a].append(b)
+        graph[b].append(a)
+    # dictionary that holds distance values for each node
+    dp = {}
+    for node in graph:
+        dp[node] = 10
+    # visited array set to False
     visited = [False] * n
-    answer = []
-    dfs(1)
-    return answer
+    # Queue
+    q = collections.deque()
+    # Add node 1
+    q.append(1)
+    visited[0] = True
+    while q:
+        cur = q.popleft()
+        for node in graph[cur]:
+            if not visited[node-1]:
+                # update distance to node
+                dp[node] = dp[cur]+ 1
+                # check in visited
+                visited[node-1] = True
+                q.append(node)
+    dp_vals = list(dp.values())
+    return dp_vals.count(max(dp_vals))
 
 n = 6
-vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
-print(solution(n, vertex))
+edge = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+print(solution(n, edge))
