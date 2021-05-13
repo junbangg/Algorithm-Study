d36c5eb20d21f7bc3d3ff6d941bbaa5c088ca32e
junbangg <wnstjr4620@naver.com>
2021-05-13 16:38:13 +0900

    Added correct solution

--- Leet Code/Odd Even Linked List/solution.py
+++ Leet Code/Odd Even Linked List/solution.py
@@ -17,3 +17,19 @@ class Solution:
             dummy.next = temp
             dummy = dummy.next
         return root.next
+
+# slow, fast approach
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        odd = head
+        even = head.next
+        evenHead = even
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even.next
+        odd.next = evenHead
+        return head
