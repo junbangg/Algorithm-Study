20da240bf9eda913ce9841e489e8b814dd275129
junbangg <wnstjr4620@naver.com>
2021-05-14 23:54:00 +0900

    Added second attempt solution

--- Leet Code/Reverse Linked List/solution.py
+++ Leet Code/Reverse Linked List/solution.py
@@ -6,6 +6,20 @@ class Solution:
             rev, rev.next, cur= cur, rev, cur.next
         return rev
 
+# Second Attempt
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        slow, fast = head, head.next
+        root = dummy = ListNode()
+        while fast:
+            slow.next, dummy.next, slow = dummy.next, slow, fast
+            fast = fast.next
+        if slow:
+            slow.next, dummy.next = dummy.next, slow
+        return root.next
+
 # Recursive Solution
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
