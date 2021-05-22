a4f36d87e9970bfd2c12079e2d3e6e7399b22ac1
junbangg <wnstjr4620@naver.com>
2021-05-23 01:41:39 +0900

    Added new solutions

--- 손코딩/빈도 수 계산과 정렬/solution.py
+++ 손코딩/빈도 수 계산과 정렬/solution.py
@@ -1,3 +1,4 @@
+
 '''
 a = [4,5,6,5,4,3]
 
@@ -31,12 +32,33 @@ def sortByCount(nums):
         while h:
             answer.append(heapq.heappop(h))
     return answer
-
+# pythonic solution: O(nlogn)
 def sortByCountImproved(nums):
     count = collections.Counter(nums)
     return sorted(nums, key = lambda x: (count[x], x))
 
-
-
+# quickSort solution : O(nlogn)
+def quickSort(nums, count, low, high):
+    def partition(nums, low, high):
+        left, pivot = low, nums[high]
+        for right in range(low, high):
+            if count[nums[right]] < count[pivot]:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            elif count[nums[right]] == count[pivot] and nums[right] < pivot:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        nums[left], nums[high] = nums[high], nums[left]
+        return left
+    if low < high:
+        p = partition(nums, low, high)
+        quickSort(nums, count, low, p-1)
+        quickSort(nums, count, p+1, high)
+    return nums
+def solution(nums):
+    if not nums:
+        return nums
+    count = collections.Counter(nums)
+    return quickSort(nums, count, 0, len(nums)-1)
 a = [4,5,6,5,4,3]
-print(sortByCountImproved(a))
+print(solution(a))
