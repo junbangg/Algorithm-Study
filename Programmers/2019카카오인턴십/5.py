9061f18a3370086adf132a5469cf3cb414f3118f
junbangg <wnstjr4620@naver.com>
2021-06-03 00:50:02 +0900

    Added new solution

--- Programmers/2019카카오인턴십/5.py
+++ Programmers/2019카카오인턴십/5.py
@@ -1,19 +1,20 @@
 def solution(k, room_number):
-    answer = []
-    vacant = [i for i in range(k)]
-    occupied = [False] * k
-    for num in room_number:
-        if not occupied[num-1]:
-            occupied[num-1] = True
-            vacant.pop(num-1)
-            answer.append(num)
+    answer, slots, stack = [0] * len(room_number), [False] * (k+1), [i for i in range(k, 0, -1)]
+    for ind, num in enumerate(room_number):
+        if not slots[num]:
+            answer[ind] = num
+            slots[num] = True
+            if num == stack[-1]:
+                stack.pop()
         else:
-            for v in vacant:
-                if v > num:
-                    occupied[v] = True
-                    vacant.pop(v)
-                    answer.append(num)
+            nextSlot = num
+            while slots[nextSlot]:
+                nextSlot = stack.pop()
+            answer[ind] = nextSlot
+            slots[nextSlot] = True
     return answer
 
 
-
+k = 10
+nums = [1,3,4,1,3,1]
+print(solution(k, nums))
