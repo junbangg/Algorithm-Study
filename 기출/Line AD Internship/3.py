480ea3bda5d33c85ef443ee70cf07e9182c539b2
junbangg <wnstjr4620@naver.com>
2021-06-05 00:42:43 +0900

    Added correct solution

--- 기출/Line AD Internship/3.py
+++ 기출/Line AD Internship/3.py
@@ -1,38 +1,29 @@
-import sys
+'''
+#1 sort ads
+#2 for each valid time frame, add candidate tasks to priority queue(max heap)
+#3 if there is something in the queue, pop the next task and update (time, totalCost, count, last)
+#4 do this until all ads are accounted for
+'''
+import heapq
 def solution(ads):
-    time, cost = 0, 0
-    ad = False
     ads.sort()
-    while ads:
-        newAd = []
-        addTime = 0
-        removeAt = -1
-        minCost = sys.maxsize
-        maxPriorityCost = -sys.maxsize
-        prioritySwitch = False
-        for i, ad in enumerate(ads):
-            if ad[0] == time and maxPriorityCost < ad[1]:
-                maxPriorityCost = ad[1]
-                newAd = ad
-                addTime = ad[0] - time + 5
-                removeAt = i
-                prioritySwitch = True
-            if not prioritySwitch:
-                potentialCost = (time-ad[0]) * ad[1]
-                if minCost > potentialCost:
-                    minCost = potentialCost
-                    removeAt, newAd = i, ad
-                    addTime = 5
-        ads.pop(removeAt)
-        if prioritySwitch:
-            cost += maxPriorityCost
+    answer, last, time, count = 0, -1, 0, 0
+    h = []
+    while count < len(ads):
+        for start, cost in ads:
+            if last < start <= time:
+                heapq.heappush(h, (-cost, start))
+        if h:
+            taskCost, startTime = heapq.heappop(h)
+            last = time
+            answer += (time - startTime) * -taskCost
+            time += 5
+            count += 1
         else:
-            cost += minCost
-        time += addTime
-        print(newAd)
-        print(time)
-        print(cost)
-    return cost
+            time += 1
+    return answer
+
+
 
 
 tc1 = [[1,3],[3,2],[5,4]] # 20
@@ -40,7 +31,7 @@ tc2 = [[0,3],[5,4]] # 0
 tc3 = [[0,1], [0,2], [6,3], [8,4]]  # 40
 tc4 = [[5,2], [2,2], [6,3], [9,2]] # 33
 
-#print(solution(tc1))
-#print(solution(tc2))
+print(solution(tc1))
+print(solution(tc2))
 print(solution(tc3))
-#print(solution(tc4))
+print(solution(tc4))
