aac251461c586f5fe858a010d02a3f75ae75d7dd
junbangg <wnstjr4620@naver.com>
2021-06-04 21:50:54 +0900

    Added correct solution

--- Codility/NailingPlanks/solution.py
+++ Codility/NailingPlanks/solution.py
@@ -1,5 +1,5 @@
 import collections, copy
-def solution(A, B, C):
+def wrongsolution(A, B, C):
     # plank dic
     plankDic = collections.defaultdict(int)
     for i in range(len(A)):
@@ -34,6 +34,31 @@ def solution(A, B, C):
         else:
             left = mid + 1
     return answer
+
+def solution(A, B, C):
+    def check(num):
+        nails = [0] * 2*(len(C)+1)
+        for nail in C[:num]:
+            nails[nail] += 1
+        for i in range(1, len(nails)):
+            nails[i] += nails[i-1]
+        for i in range(len(A)):
+            if nails[B[i]] - nails[A[i]-1] == 0:
+                return False
+        return True
+    # binarySearch 
+    left, right = 1, len(A)
+    answer = -1
+    while left <= right:
+        mid = left + (right - left) // 2
+        # if all planks can be nailed with mid amount
+        if check(mid):
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return answer
+
 
 A = [1,4,5,8]
 B = [4,5,9,10]
