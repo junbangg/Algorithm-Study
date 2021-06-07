73ec95733bf94b8912d7560c6aed935c947c0f5f
junbangg <wnstjr4620@naver.com>
2021-06-07 17:50:47 +0900

    Added 백준 problem

---
+++ 백준/음계/solution.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    ascending, descending = True, True
+    for i in range(1, len(arr)):
+        if arr[i-1] < arr[i]:
+            descending = False
+        else:
+            ascending = False
+    if ascending:
+        print('ascending')
+    elif descending:
+        print('descending')
+    else:
+        print('mixed')
+
+testCase = list(map(int, input().split()))
+solution(testCase)
+
