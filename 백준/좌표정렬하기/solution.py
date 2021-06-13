12d848037d421c75a32f23b0f1182413c672cab8
junbangg <wnstjr4620@naver.com>
2021-06-13 15:53:01 +0900

    Added new problem

---
+++ 백준/좌표정렬하기/solution.py
@@ -0,0 +1,10 @@
+n = int(input())
+result = []
+for _ in range(n):
+    x, y = input().split(' ')
+    result.append((int(x), int(y)))
+result.sort(key = lambda x: (x[0], x[1]))
+for pt in result:
+    print(''.join(str(pt[0]) + ' ' + str(pt[1])))
+
+
