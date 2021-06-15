fd457cf531258690b7ec98e7ac1b9c18578f2c50
junbangg <wnstjr4620@naver.com>
2021-06-15 13:32:04 +0900

    Optimized code

--- 백준/이름궁합/solution.py
+++ 백준/이름궁합/solution.py
@@ -1,11 +1,12 @@
+from collections import deque
 n, m = map(int, list(input().split(' ')))
 first, second = list(input().split(' '))
 # dictionary
 dic = {'A': 3, 'B': 2, 'C': 1, 'D': 2, 'E': 4, 'F': 3, 'G': 1, 'H': 3, 'I': 1,
         'J': 1, 'K': 3, 'L': 1, 'M': 3, 'N': 2, 'O': 1, 'P': 2, 'Q': 2, 'R': 2,
         'S': 1, 'T': 2, 'U': 1, 'V': 1, 'W': 1, 'X': 2, 'Y': 2, 'Z': 1}
-# combine
-combine = []
+# combine and convert to int
+combine = deque()
 i, j = 0, 0
 while i < len(first) and j < len(second):
     combine.append(dic[first[i]])
@@ -18,13 +19,14 @@ while i < len(first):
 while j < len(second):
     combine.append(dic[second[j]])
     j += 1
+# calculate
 while len(combine) > 2:
     prev = combine[0]
     for i in range(1,len(combine)):
         temp = combine[i]
         combine[i] = (combine[i] + prev) % 10
         prev = temp
-    combine.pop(0)
+    combine.popleft()
 if combine[0] == 0:
-    combine.pop(0)
+    combine.popleft()
 print(''.join(list(map(str, combine))) + '%')
