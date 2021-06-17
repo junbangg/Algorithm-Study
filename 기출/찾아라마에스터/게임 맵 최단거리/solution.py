d080a38e686aed16587b3624f54c31c28d2ba3e2
junbangg <wnstjr4620@naver.com>
2021-06-17 11:28:12 +0900

    Added solution

--- 기출/찾아라마에스터/게임 맵 최단거리/solution.py
+++ 기출/찾아라마에스터/게임 맵 최단거리/solution.py
@@ -1,28 +1,33 @@
 from collections import deque
 def solution(maps):
-    visited = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    X, Y = len(maps), len(maps[0])
+    visited = [[False for _ in range(X)] for _ in range(Y)]
+
+    def check(x, y):
+        if x >= 0 and x < X and y >= 0 and y < Y:
+            if maps[x][y] == 1 and not visited[x][y]:
+                return True
+        return False
+    
     # directions
     # N, S, W, E
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     q = deque()
-    q.append((0, 0, 0))
+    q.append((0, 0, 1))
+    visited[0][0] = True
     while q:
-        x, y, prev = q.popleft()
-        print("{}, {} = {}".format(x, y, prev+1))
-        if not visited[x][y]:
-            visited[x][y] = True
-            maps[x][y] += prev
-        else:
-            if maps[x][y] > prev + 1:
-                maps[x][y] = prev + 1
+        x, y, cost = q.popleft()
+        # add N, S, W, E to queue
         for i in range(4):
             nextX, nextY = x + dx[i], y + dy[i]
-            if nextX < 0 or nextX >= 5 or nextY < 0 or nextY >= 5 or maps[nextX][nextY] == 0:
-                continue
-            elif not visited[nextX][nextY] or maps[nextX][nextY] > prev + 1:
-                q.append((nextX, nextY, maps[x][y]))
-    return -1 if maps[4][4] == 1 else maps[4][4]
+            # check if valid coordinates
+            if check(nextX, nextY):
+                visited[nextX][nextY] = True
+                if nextX == X - 1 and nextY == Y - 1:
+                    return cost + 1
+                q.append((nextX, nextY, cost + 1))
+    return -1
 
 
 
--- 기출/찾아라마에스터/게임 맵 최단거리/solution2.py
+++ 기출/찾아라마에스터/게임 맵 최단거리/solution2.py
@@ -1,23 +1,27 @@
+from collections import deque
 def solution(maps):
-
-    answers = []
+    X, Y = len(maps), len(maps[0])
+    visited = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    # N, S, W, E
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-
-    def dfs(x, y, count):
-        if x == len(maps) - 1 and y == len(maps[0]) - 1:
-            answers.append(count)
-            return
-        maps[x][y] = 0
+    q = deque()
+    q.append((0, 0, 0))
+    while q:
+        x, y, prev = q.popleft()
+        if not visited[x][y]:
+            visited[x][y] = True
+            maps[x][y] += prev
+        else:
+            if maps[x][y] > prev + 1:
+                maps[x][y] = prev + 1
         for i in range(4):
             nextX, nextY = x + dx[i], y + dy[i]
-            if nextX >= 0 and nextX < len(maps) and nextY >= 0 and nextY < len(maps[0]) and maps[nextX][nextY] == 1:
-                dfs(nextX, nextY, count + 1)
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-            if maps[i][j] == 1:
-                dfs(i, j, 1)
-    return min(answers)
+            if nextX < 0 or nextX >= X or nextY < 0 or nextY >= Y or maps[nextX][nextY] == 0:
+                continue
+            elif not visited[nextX][nextY] or maps[nextX][nextY] > prev + 1:
+                q.append((nextX, nextY, maps[x][y]))
+    return -1 if maps[X-1][Y-1] == 1 else maps[X-1][Y-1]
 
 
         
