845c2a4b3a97f08f28685e32c3f053653d17d90a
junbangg <wnstjr4620@naver.com>
2021-06-24 01:02:57 +0900

    Added solution

--- 백준/로봇청소기/solution.py
+++ 백준/로봇청소기/solution.py
@@ -1,45 +1,28 @@
 import sys
 N, M = list(map(int, sys.stdin.readline().split()))
 r, c, d = list(map(int, sys.stdin.readline().split()))
-direction = {0: 0,
-             1: 3,
-             3: 1}
-d = direction[d]
 map = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-'''
-dir = { 0 : (0, -1), #N
-        1: (-1, 0), #E
-        2: (0, 1), #S
-        3: (1, 0) } #W
-'''
-dir = { 0 : (0, -1, 1), #N
-        1 : (1, 0, 2), #W
-        2 : (0, 1, 3), #S
-        3 : (-1, 0, 0) }#E
+#N E S W
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 answer = 0
 def clean(x, y, d):
     global answer
     if map[x][y] == 0:
         answer += 1
-        map[x][y] = -1
-    count = 0
-    while count < 4:
-        dx, dy, dd = dir[d]
-        nx, ny = x + dx, y + dy
-        if nx >= 0 and nx < N and ny >= 0 and ny < M and map[nx][ny] < 1:
-            clean(nx, ny, dd)
-        count += 1
-        d = dd
-    return
+        map[x][y] = 2
+    for _ in range(4):
+        left = (d - 1) % 4
+        nx, ny = x + dx[left], y + dy[left]
+        if map[nx][ny] == 0:
+            clean(nx, ny, left)
+            return
+        d = left
+    # 후진
+    # 한바퀴 돌아서 d 가 다시 처음 d 가 됨
+    nx, ny = x - dx[d], y - dy[d]
+    if map[nx][ny] == 1:
+        return
+    clean(nx, ny, d)
 clean(r, c, d)
 print(answer)
-'''        
-5 5 
-2 3 0
-1 1 0 1 1
-1 0 0 0 1
-1 0 0 0 1
-1 0 1 1 1
-1 0 0 0 1
-'''
-
