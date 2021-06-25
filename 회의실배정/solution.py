674c925135f3e6250fd23a80466d8e75832765cc
junbangg <wnstjr4620@naver.com>
2021-06-25 16:09:38 +0900

    Added new problem

---
+++ 회의실배정/solution.py
@@ -0,0 +1,24 @@
+# 1931
+import heapq
+
+n = int(input())
+
+h = []
+# make heap
+for _ in range(n):
+    start, end = list(map(int, input().split()))
+    heapq.heappush(h, [end, start])
+
+# solve
+count = 0
+# [start, end]
+prev = [0, 0]
+while h:
+    end, start = heapq.heappop(h)
+    if prev[1] <= start:
+        count += 1
+        prev = [start, end]
+print(count)
+
+
+
