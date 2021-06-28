b002f7707ac459014b4014e7a6b4a8bed71e4e33
junbangg <wnstjr4620@naver.com>
2021-06-29 01:44:51 +0900

    Added solution

--- 백준/2110/solution.py
+++ 백준/2110/solution.py
@@ -1,35 +1,27 @@
 N, C = map(int, input().split())
 points = sorted([int(input()) for _ in range(N)])
-answer = set()
 # edge case
-if C == 2:
-    print(points[-1] - points[0])
 
-def binarySearch(left, right, x, y):
-    # left
-    ll, lr = left, right
-    lcount = 0
-    while ll <= lr:
-        pivot = ll + (lr - ll) // 2
-        if lcount <= x:
-            answer.add(points[pivot])
-            lcount += 1
-            right = pivot - 1
+def count(gap):
+    prev, count = points[0], 1
+    for i in range(1, len(points)):
+        if points[i] - prev >= gap:
+            count += 1
+            prev = points[i]
+    return count
+
+def binarySearch(left, right):
+    answer = 0
+    while left <= right:
+        gap = left + (right - left) // 2
+        cnt = count(gap)
+        if cnt >= C:
+            left = gap + 1
+            answer = gap
         else:
-            break
-    # right
-    rl, rr = left, right
-    rcount = 0
-    while rl <= rr:
-        pivot = rl + (rl - rr) // 2
-        if rcount <= y:
-            answer.add(points[pivot])
-            rcount += 1
-            left = pivot + 1
-        else:
-            break
+            right = gap - 1
+    return answer
 
-x, y = C // 2, C - C // 2
 
-binarySearch(0, len(points)-1, x+1, y+1)
-print(answer)
+left, right = 1, points[-1] - points[0]
+print(binarySearch(left, right))
