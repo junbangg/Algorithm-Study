c73c25daa71a9dc0762d392d49a9b283a62d0d72
junbangg <wnstjr4620@naver.com>
2021-07-21 15:31:53 +0900

    Added new problem

---
+++ 백준/1202/solution.py
@@ -0,0 +1,20 @@
+import sys, heapq
+input = sys.stdin.readline
+N, K = map(int, input().split())
+h, gems, bags = [], [], []
+for _ in range(N):
+    weight, cost = map(int, input().split())
+    heapq.heappush(gems, (weight, cost))
+for _ in range(K):
+    heapq.heappush(bags, int(input()))
+
+candidates = []
+answer = 0
+for _ in range(K):
+    bag = heapq.heappop(bags)
+    while gems and bag >= gems[0][0]:
+        gem_weight, gem_cost = heapq.heappop(gems)
+        heapq.heappush(candidates, -gem_cost)
+    if candidates:
+        answer += -heapq.heappop(candidates)
+print(answer)
