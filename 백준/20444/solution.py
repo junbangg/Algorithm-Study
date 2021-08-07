9a234cd2e0de1b677a3e25e4f1a1f4abe4ff1e1f
junbangg <wnstjr4620@naver.com>
2021-08-07 14:48:26 +0900

    Added swift solution

--- 백준/20444/solution.py
+++ 백준/20444/solution.py
@@ -14,23 +14,3 @@ while left < right:
     else:
         right = mid
 print('NO')
-# import sys
-# input = sys.stdin.readline
-# N, K = map(int, input().split())
-
-# left, right = 0, N // 2 + 1
-# dp = [0] * (right+1)*(right+1)
-# while left <= right:
-#     mid = left + (right - left) // 2
-#     cuts = (mid+1)*(N-mid+1)
-#     if dp[cuts] == 1:
-#         print('NO')
-#         exit(0)
-#     if cuts == K:
-#         print('YES')
-#         exit(0)
-#     elif cuts < K:
-#         left = mid
-#     else:
-#         right = mid 
-#     dp[cuts] = 1
---
+++ 백준/20444/solution.swift
@@ -0,0 +1,27 @@
+import Foundation
+//Input
+let nk = readLine()!.split(separator: " ").compactMap{Int($0)}
+let N = nk[0]
+let K = nk[1]
+
+//Binary Search
+func binarySearch(_left: Int, _right: Int) -> Bool {
+    var left = _left
+    var right = _right
+    while left < right {
+        let mid = left + (right - left) / 2
+        let cuts = (mid+1) * (N-mid+1)
+        if (cuts == K) {
+            return true 
+        } else if(cuts < K) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    return false
+}
+
+// Solve
+let answer = binarySearch(_left: 0, _right: N/2 + 1)
+print(answer == true ? "YES" : "NO")
