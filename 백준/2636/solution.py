fabc4dedb62f3cc9dc485cfc029e54f63211b93e
junbangg <wnstjr4620@naver.com>
2021-08-24 13:29:42 +0900

    modified code

--- 백준/2636/solution.py
+++ 백준/2636/solution.py
@@ -1,28 +1,30 @@
-from collections import deque
+import sys, collections
+input = sys.stdin.readline
 N, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
 
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-def bfs(x, y):
+def bfs(src_x, src_y):
     global count
-    q = deque()
-    q.append((x, y))
+    q = collections.deque()
+    q.append((src_x, src_y))
     while q:
-        cur_x, cur_y = q.popleft()
+        x, y = q.popleft()
         for i in range(4):
-            nx, ny = cur_x + dx[i], cur_y + dy[i]
-            if nx >= 0 and nx < N and ny >= 0 and ny < M and visited[nx][ny] == 0:
-                if grid[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    q.append((nx, ny))
-                elif grid[nx][ny] == 1:
-                    count += 1
-                    visited[nx][ny] = 1
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                # 치즈
+                if grid[nx][ny] == 1:
                     grid[nx][ny] = 0
+                    count += 1
+                # 새로운 공기
+                else:
+                    q.append((nx, ny))
 
-prev, time = 0, 0
+prev = time = 0
 while True:
     count = 0
     visited = [[0 for _ in range(M)] for _ in range(N)]
@@ -32,6 +34,5 @@ while True:
         break
     prev = count
     time += 1
-
 print(time)
 print(prev)
