262b74d3f003eca3a9ba31444e92246e57b88ea3
junbangg <wnstjr4620@naver.com>
2021-08-28 23:33:22 +0900

    Changed code

--- 백준/16973/solution.py
+++ 백준/16973/solution.py
@@ -1,4 +1,4 @@
-import sys, collections
+import sys, collections, copy
 input = sys.stdin.readline
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
@@ -9,20 +9,18 @@ def moveSquare(nx, ny, square):
     for i in range(len(square)):
         square[i][0] += nx
         square[i][1] += ny
+    print('moved square')
     print(square)
+    print('\n')
     return square
 
-def isValid(nx, ny, square):
+def isValid(nx, ny, square, visited):
+    if visited[square[0][0] + nx][square[0][1] + ny] == 1:
+        return False
     for x, y in square:
         if 0 > x + nx or N <= x + nx or 0 > y + ny or M <= y + ny or board[x + nx][y + ny] == 1:
             return False
     return True
-    # for i in range(H):
-    #     for j in range(W):
-    #         nx, ny = x + i, y + j
-    #         if 0 > nx or N <= nx or 0 > ny or M <= ny or board[nx][ny] == 1:
-    #             return False
-    # return True
 
 def printBoard(board):
     for i in range(len(board)):
@@ -33,18 +31,32 @@ dy = [1, -1, 0, 0]
 
 def bfs(src_x, src_y, dest_x, dest_y, square):
     q = collections.deque()
-    visited = [[INF] * M for _ in range(N)]
-    visited[src_x][src_y] = 0
-    q.append((src_x, src_y, square))
+    visited = [[0] * M for _ in range(N)]
+    visited[src_x][src_y] = 1
+    q.append((0, square))
     while q:
-        cur_x, cur_y, cur_square = q.popleft()
+        print('popped from queue')
+        print(q)
+        cur_dist, cur_square = q.popleft()
+        # cur_x, cur_y = cur_square[0][0], cur_square[0][1]
         for i in range(4):
-            nx, ny = cur_x + dx[i], cur_y + dy[i]
-            if isValid(dx[i], dy[i], cur_square) and visited[cur_x][cur_y] + 1 < visited[nx][ny]:
-                visited[nx][ny] = visited[cur_x][cur_y] + 1
-                q.append((nx, ny, moveSquare(dx[i], dy[i], cur_square)))
-    printBoard(visited)
-    return visited[dest_x][dest_y]
+            nx, ny = cur_square[0][0] + dx[i], cur_square[0][1] + dy[i]
+            print(cur_square)
+            if isValid(dx[i], dy[i], cur_square, visited):
+                print(dx[i], dy[i], 'moving direction')
+                print(nx, ny, 'valid coord\n')
+                visited[nx][ny] = 1
+                print('distance is', cur_dist+1)
+                printBoard(visited)
+                if nx == dest_x and ny == dest_y:
+                    return cur_dist + 1
+                q.append((cur_dist + 1, moveSquare(dx[i], dy[i], copy.deepcopy(cur_square))))
+            else:
+                print(dx[i], dy[i], 'moving direction')
+                print(nx, ny, 'invalid coord\n')
+                print('invalid')
+                printBoard(visited)
+    return -1
 # square 좌표들
 square = []
 # for i in range(H):
@@ -57,5 +69,4 @@ for i in range(H):
     else:
         square.append([SX-1 + i, 0])
         square.append([SX-1 + i, W-1])
-print(square)
 print(bfs(SX-1, SY-1, FX-1, FY-1, square))
