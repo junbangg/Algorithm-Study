be7279868d214b4d16f6647deae11b7e984d2578
junbangg <wnstjr4620@naver.com>
2021-09-06 15:34:57 +0900

    modified solution

--- 기출/KAKAO 2019/후보키/solution.py
+++ 기출/KAKAO 2019/후보키/solution.py
@@ -4,23 +4,23 @@ def solution(relation):
     relation = list(map(list,zip(*relation)))
     rows = [i for i in range(len(relation))]
     candidates, combs = [], []
-
+    # 모든 가능한 후보키 조합 생성
     for i in range(1, len(relation) + 1):
         for comb in list(combinations(rows, i)):
             combs.append(comb)
+    # 모든 조합에 대한 최소성, 유일성 체크
     for comb in combs:
-        check = []
         # 최소성 체크
-        cont = False
+        flag = False
         for cand in candidates:
-            for c in comb:
-                if c in cand:
-                    cont = True
+            for c in combinations(comb, len(cand)):
+                if c == cand:
+                    flag = True
                     break
-        if cont:
+        if flag:
             continue
-
         # 유일성 체크
+        check = []
         for col in range(len(relation[0])):
             tup = ()
             for row in comb:
@@ -29,5 +29,5 @@ def solution(relation):
         if len(check) == len(set(check)):
             candidates.append(comb)
             answer += 1
+    # print('candidates', candidates)
     return answer
-
