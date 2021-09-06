101181e6e2740b921ff0e48935ac2972b3664eed
junbangg <wnstjr4620@naver.com>
2021-09-06 13:09:21 +0900

    Added new problem

---
+++ 백준/5052/solution.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+tc = int(input())
+for _ in range(tc):
+    N = int(input())
+    # 입력 숫자들 정렬
+    nums = sorted([input().rstrip() for _ in range(N)])
+    # 일관성이 계속 유지되는지 확인
+    unique = True 
+    for i in range(1, len(nums)):
+        length = len(nums[i-1])
+        if nums[i-1] in nums[i][:length]:
+            unique = False
+            break
+    if unique:
+        print('YES')
+    else:
+        print('NO')
+
+
+
+            
+
