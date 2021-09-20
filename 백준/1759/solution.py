31141bfabf76a6651588dda56be617ee9d644955
junbangg <wnstjr4620@naver.com>
2021-09-20 18:31:33 +0900

    Added new problem

---
+++ 백준/1759/solution.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+L, C = map(int, input().split())
+letters = sorted(input().rstrip().split())
+
+answers = []
+
+def isValid(s):
+    vowels = 'aeiou'
+    vowel = nonVowel = 0
+    for c in s:
+        if c in vowels:
+            vowel += 1
+        else:
+            nonVowel += 1
+    return vowel >= 1 and nonVowel >= 2
+
+def recurse(ind, s):
+    if len(s) == L:
+        if isValid(s):
+            answers.append(s)
+        return
+    for i in range(ind+1, C):
+        recurse(i, s + letters[i])
+
+for i in range(C):
+    recurse(i, letters[i])
+
+for a in answers:
+    print(a)
