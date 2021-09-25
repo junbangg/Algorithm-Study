c48d8cb653535948f6070d8a55dfe098942af2f8
junbangg <wnstjr4620@naver.com>
2021-09-25 15:49:57 +0900

    Added new problem

--- 백준/1253/solutioin.py
+++ 백준/1253/solutioin.py
@@ -3,8 +3,9 @@ input = sys.stdin.readline
 N = int(input())
 nums = sorted(list(map(int, input().split())))
 dic = collections.Counter(nums)
+# -2: 2
 answer = 0
-for i in range(N):
+for i in range(N): # 7 번 
     if dic[nums[i]] != 0:
         left, right = 0, N-1
         while left < right:
@@ -15,8 +16,9 @@ for i in range(N):
                 right -= 1
                 continue
             if nums[i] == nums[left] + nums[right]:
-                answer += 1
-                dic[nums[i]] -= 1
+                # answer += 1
+                answer += dic[nums[i]]
+                dic[nums[i]] = 0
                 break
             elif nums[i] < nums[left] + nums[right]:
                 right -= 1
@@ -24,8 +26,11 @@ for i in range(N):
                 left += 1
 print(answer)
 
-# 7
-# -4 -2 -2 -1 1 2 4
+7
+-4 -2 -2 -1 1 2 4
 
-# 7
-# -3 -3 -1 0 2 3 5
+
+-4 -2 -2 -2 -2 -2 2
+
+7
+-3 -3 -1 0 2 3 5 
