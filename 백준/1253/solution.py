14054bfa117970ae08763dace79cc9b68a0634ae
junbangg <wnstjr4620@naver.com>
2021-09-25 16:10:49 +0900

    Changed file name

--- 백준/1253/solution.py
+++ 백준/1253/solution.py
@@ -3,9 +3,8 @@ input = sys.stdin.readline
 N = int(input())
 nums = sorted(list(map(int, input().split())))
 dic = collections.Counter(nums)
-# -2: 2
 answer = 0
-for i in range(N): # 7 번 
+for i in range(N):
     if dic[nums[i]] != 0:
         left, right = 0, N-1
         while left < right:
@@ -16,7 +15,6 @@ for i in range(N): # 7 번
                 right -= 1
                 continue
             if nums[i] == nums[left] + nums[right]:
-                # answer += 1
                 answer += dic[nums[i]]
                 dic[nums[i]] = 0
                 break
@@ -26,11 +24,11 @@ for i in range(N): # 7 번
                 left += 1
 print(answer)
 
-7
--4 -2 -2 -1 1 2 4
+# 7
+# -4 -2 -2 -1 1 2 4
 
+# 7
+# -4 -2 -2 -2 -2 -2 2
 
--4 -2 -2 -2 -2 -2 2
-
-7
--3 -3 -1 0 2 3 5 
+# 7
+# -3 -3 -1 0 2 3 5 
