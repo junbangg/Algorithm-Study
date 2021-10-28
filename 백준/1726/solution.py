966b6efb0739615e84c9e1f7b4d8bbbb0a4b6363
junbangg <wnstjr4620@naver.com>
2021-10-28 23:39:48 +0900

    final attempt

--- 백준/1726/solution.py
+++ 백준/1726/solution.py
@@ -5,9 +5,9 @@ _map = [input().split() for _ in range(N)]
 src = list(map(int, input().split()))
 dest = list(map(int, input().split()))
 
-# 동쪽이 1, 서쪽이 2, 남쪽이 3, 북쪽이 4
-dx = [-1, 1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 
 def getMinDirectionCommand(before, after):
     if abs(before-after) == 3:
@@ -24,31 +24,45 @@ def printMap(board):
 def bfs(src_x, src_y, src_dir, dest_x, dest_y, dest_dir): # 전부 1 뺴기
     visited = [[False] * M for _ in range(N)]
     visited[src_x][src_y] = True
-    q = collections.deque([(src_x, src_y, src_dir, 0, 0)]) # x,  y, 방향, cmds, leap
+    q = collections.deque([(src_x, src_y, src_dir, 0, 0, False)]) # x,  y, 방향, cmds, leap
 
     while q:
-        cur_x, cur_y, cur_dir, cur_cost, leap = q.popleft()
-        if cur_x == dest_x and cur_y == dest_y:
-            if cur_dir != dest_dir:
-                print("같은 방향 아님")
-                cur_cost += getMinDirectionCommand(cur_dir, dest_dir)
-            print("도착스: cur_dir, nxt_cost", cur_dir, cur_cost)
-            return cur_cost
-        for i in range(4):
-            nx, ny = cur_x + dx[i], cur_y + dy[i]
-            minDirectionCommands = getMinDirectionCommand(cur_dir, i)
+        cur_x, cur_y, cur_dir, cur_cost, leap, leapSwitch = q.popleft()
+        # 0, 0, 4, 3
+        for nxt_dir in range(4):
+            nx, ny = cur_x + dx[nxt_dir], cur_y + dy[nxt_dir]
+            minDirectionCommands = getMinDirectionCommand(cur_dir, nxt_dir)
             nxt_cost = cur_cost + minDirectionCommands
+            # 방향이 바뀌었다는 뜻
             if minDirectionCommands > 0:
                 nxt_cost += 1
+                leap = 0
+                leapSwitch = False
             else:
-                leap += 1
-                if leap > 3:
+                if not leapSwitch:
                     nxt_cost += 1
-                    leap = 0
-            #nxt_cost = cur_cost + minimumDirectionCommand(cur_dir, i) + 1
+                leapSwitch = True
+                leap += 1
+            if leap == 3:
+                #nxt_cost += 1
+                leap = 0
+                leapSwitch = False
             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and _map[nx][ny] == '0':
                 visited[nx][ny] = True
-                print("nx, ny, nxt_cost", nx, ny, nxt_cost)
-                q.append((nx, ny, i, nxt_cost, leap))
+                print("nx, ny, nxt_cost, leap", nx, ny, nxt_cost, leap)
+                if nx == dest_x and ny == dest_y:
+                    return nxt_cost + getMinDirectionCommand(nxt_dir, dest_dir)
+                q.append((nx, ny, nxt_dir, nxt_cost, leap, leapSwitch))
 
-print(bfs(src[0]-1, src[1]-1, src[2]-1, dest[0]-1, dest[1]-1, dest[2]-1))
+def convertDirection(num):
+    if num == 1:
+        return 1
+    if num == 2:
+        return 3
+    if num == 3:
+        return 2
+    if num == 4:
+        return 0
+
+print(bfs(src[0]-1, src[1]-1, src[2]-1, dest[0]-1, dest[1]-1, convertDirection(dest[2])))
+
