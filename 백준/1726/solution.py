12ae61fe10777aeadd419270cdde4621925323c9
junbangg <wnstjr4620@naver.com>
2021-10-30 01:31:44 +0900

    added todo

--- 백준/1726/solution.py
+++ 백준/1726/solution.py
@@ -1,4 +1,4 @@
-import sys, heapq
+import sys, collections
 input = sys.stdin.readline
 N, M = map(int, input().split())
 _map = [input().split() for _ in range(N)]
@@ -14,39 +14,36 @@ def getMinDirectionCommand(before, after):
     else:
         return abs(before-after)
 
+test = [(3, 1), (3, 0), (2, 0), (1, 0), (0, 0), (0, 1), (0, 2), (0, 3), (3, 1)]
+test2 = [(3, 1), (4, 1), (4, 2), (4, 3), (4,4), (4, 5), (3, 5), (2, 5), (2, 4)]
 def bfs(src_x, src_y, src_dir, dest_x, dest_y, dest_dir): # 전부 1 뺴기
     visited = [[False] * M for _ in range(N)]
     visited[src_x][src_y] = True
-    # q = collections.deque([(src_x, src_y, src_dir, 0, 0)]) # x,  y, 방향, cmds, leap
-    q = []
-    heapq.heappush(q, [0, src_x, src_y, src_dir, 0])
-
+    q = collections.deque([(src_x, src_y, src_dir, 0, 0)]) # x,  y, 방향, cmds, leap
     while q:
-        # cur_x, cur_y, cur_dir, cur_cost, leap = q.popleft()
-        cur_cost, cur_x, cur_y, cur_dir, leap = heapq.heappop(q)
+        cur_x, cur_y, cur_dir, cur_cost, cur_leap = q.popleft()
         for nxt_dir in range(4):
             nx, ny = cur_x + dx[nxt_dir], cur_y + dy[nxt_dir]
-            minDirectionCommands = getMinDirectionCommand(cur_dir, nxt_dir)
-            nxt_cost = cur_cost + minDirectionCommands
-            if minDirectionCommands > 0:
-                nxt_cost += 1
-            else:
-                if leap == 0:
-                    nxt_cost += 1
-                leap += 1
-            if leap == 3:
-                leap = 0
             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and _map[nx][ny] == '0':
                 visited[nx][ny] = True
-                print("nx, ny, nxt_cost, leap", nx, ny, nxt_cost, leap)
+                minDirectionCommands = getMinDirectionCommand(cur_dir, nxt_dir)
+                nxt_cost = cur_cost + minDirectionCommands
+                nxt_leap = cur_leap
+                if minDirectionCommands > 0:
+                    nxt_cost += 1
+                    nxt_leap = 0
+                else:
+                    if nxt_leap == 0:
+                        nxt_cost += 1
+                nxt_leap += 1
+                if nxt_leap == 3:
+                    nxt_leap = 0
+                if (nx, ny) in test2:
+                    print("nx, ny, nxt_cost, leap", nx, ny, nxt_cost, nxt_leap)
                 if nx == dest_x and ny == dest_y:
                     # print("도착: cost, cur_dir, dest_dir, directionCost", nxt_cost, nxt_dir, dest_dir, getMinDirectionCommand(nxt_dir, dest_dir))
                     return nxt_cost + getMinDirectionCommand(nxt_dir, dest_dir)
-                # if leap > 0:
-                    # q.appendleft((nx, ny, nxt_dir, nxt_cost, leap))
-                else:
-                    # q.append((nx, ny, nxt_dir, nxt_cost, leap))
-                    heapq.heappush(q, [nxt_cost, nx, ny, nxt_dir, leap])
+                q.append((nx, ny, nxt_dir, nxt_cost, nxt_leap))
 
 def convertDirection(num):
     if num == 1:
@@ -68,3 +65,12 @@ print(bfs(src[0]-1, src[1]-1, src[2]-1, dest[0]-1, dest[1]-1, convertDirection(d
 # 1 0 0 0 0 0
 # 4 2 3
 # 3 5 1
+
+# 5 6
+# 0 0 0 0 0 0
+# 0 1 1 0 1 0
+# 0 1 0 0 0 0
+# 0 0 1 1 1 0
+# 1 0 0 0 0 0
+# 4 2 3
+# 1 6 3
--- 백준/1726/solution2.py
+++ 백준/1726/solution2.py
@@ -1,4 +1,5 @@
-import sys, collections, heapq
+# 3칸"씩" 이동해야됨;
+import sys, heapq
 input = sys.stdin.readline
 N, M = map(int, input().split())
 _map = [input().split() for _ in range(N)]
@@ -14,30 +15,40 @@ def getMinDirectionCommand(before, after):
     else:
         return abs(before-after)
 
+def printBoard(board):
+    for i in range(len(board)):
+        print(*board[i])
+
 def bfs(src_x, src_y, src_dir, dest_x, dest_y, dest_dir): # 전부 1 뺴기
-    visited = [[False] * M for _ in range(N)]
-    visited[src_x][src_y] = True
+    visited = [[float('inf')] * M for _ in range(N)]
+    visited[src_x][src_y] = 0
     q = []
     heapq.heappush(q, [0, src_x, src_y, src_dir, 0])
     while q:
-        cur_cost, cur_x, cur_y, cur_dir, leap = heapq.heappop(q)
+        cur_cost, cur_x, cur_y, cur_dir, cur_leap = heapq.heappop(q)
         for nxt_dir in range(4):
             nx, ny = cur_x + dx[nxt_dir], cur_y + dy[nxt_dir]
-            minDirectionCommands = getMinDirectionCommand(cur_dir, nxt_dir)
-            nxt_cost = cur_cost + minDirectionCommands
-            if minDirectionCommands > 0:
-                nxt_cost += 1
-            else:
-                if leap == 0:
+            if 0 <= nx < N and 0 <= ny < M and _map[nx][ny] == '0':
+                minDirectionCommands = getMinDirectionCommand(cur_dir, nxt_dir)
+                nxt_cost = cur_cost + minDirectionCommands
+                nxt_leap = cur_leap
+                if minDirectionCommands > 0:
                     nxt_cost += 1
-                leap += 1
-            if leap == 3:
-                leap = 0
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and _map[nx][ny] == '0':
-                visited[nx][ny] = True
-                if nx == dest_x and ny == dest_y:
-                    return nxt_cost + getMinDirectionCommand(nxt_dir, dest_dir)
-                heapq.heappush(q, [nxt_cost, nx, ny, nxt_dir, leap])
+                    nxt_leap = 0
+                else:
+                    if nxt_leap == 0:
+                        nxt_cost += 1
+                nxt_leap += 1
+                if nxt_leap == 3:
+                    nxt_leap = 0
+                # if nx == dest_x and ny == dest_y:
+                    # print("도착스: nx, ny, 방향 적용 전, 방향 적용 후", cur_x, cur_y, nxt_cost, nxt_cost + getMinDirectionCommand(nxt_dir, dest_dir))
+                if nxt_cost < visited[nx][ny]:
+                    visited[nx][ny] = nxt_cost
+                    if nx == dest_x and ny == dest_y:
+                        visited[nx][ny] += getMinDirectionCommand(nxt_dir, dest_dir)
+                    heapq.heappush(q, (nxt_cost, nx, ny, nxt_dir, nxt_leap))
+    return visited[dest_x][dest_y]
 
 def convertDirection(num):
     if num == 1:
@@ -51,3 +62,29 @@ def convertDirection(num):
 
 print(bfs(src[0]-1, src[1]-1, src[2]-1, dest[0]-1, dest[1]-1, convertDirection(dest[2])))
 
+# 5 6
+# 0 0 0 0 0 0
+# 0 1 1 0 1 0
+# 0 1 0 0 0 0
+# 0 0 1 1 1 0
+# 1 0 0 0 0 0
+# 4 2 3
+# 3 5 1
+
+# 5 6
+# 0 0 0 0 0 0
+# 0 1 1 0 1 0
+# 0 1 0 0 0 0
+# 0 0 1 1 1 0
+# 1 0 0 0 0 0
+# 4 2 3
+# 1 6 3
+
+# 5 6
+# 0 0 0 0 0 0
+# 0 1 1 0 1 0
+# 0 1 0 0 0 0
+# 0 0 1 1 1 0
+# 1 0 0 0 0 0
+# 4 2 3
+# 1 6 4
