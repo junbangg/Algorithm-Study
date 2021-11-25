84e122e7f8aba5c7a2c4f16bdaba0fa2af7c6d3d
junbangg <wnstjr4620@naver.com>
2021-11-26 01:06:53 +0900

    second attmpet

---
+++ 백준/1092/solution2.py
@@ -0,0 +1,27 @@
+N = int(input())
+cranes = list(map(int, input().split()))
+M = int(input())
+boxes = list(map(int, input().split()))
+
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
+# edge case
+if cranes[0] < boxes[0]:
+    print(-1)
+    exit(0)
+
+crane_box = [0] * N
+checked = [False] * M
+
+count = time = 0
+while count < M:
+    for i in range(N):
+        while crane_box[i] < M:
+            if not checked[crane_box[i]] and boxes[crane_box[i]] <= cranes[i]:
+                checked[crane_box[i]] = True
+                crane_box[i] += 1
+                count += 1
+                break
+            crane_box[i] += 1
+    time += 1
+print(time)
