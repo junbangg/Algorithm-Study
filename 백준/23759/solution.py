430992a0190f5ee8618db14c96acb6fa4679df28
junbangg <wnstjr4620@naver.com>
2021-11-30 01:02:28 +0900

    Modified solution

--- 백준/23759/solution.py
+++ 백준/23759/solution.py
@@ -2,8 +2,10 @@ import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
 words = [input().rstrip() for _ in range(N)]
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 dp = [[0] * N for _ in range(26)]
 
+# set dp
 for letter in words[0]:
     letterIndex = ord(letter) - 97
     dp[letterIndex][0] += 1
@@ -11,12 +13,13 @@ for letter in words[0]:
 answer = -float('inf')
 for i in range(26):
     for j in range(1, len(words)):
-        for letter in words[j]:
-            letterIndex = ord(letter) - 97
-            if i == letterIndex:
-                dp[i][j] = dp[i][j-1] + 1
-                answer = max(answer, dp[i][j])
-            else:
-                dp[i][j] = dp[i][j-1]
+        if alphabet[i] in words[j]:
+            dp[i][j] = dp[i][j-1] + 1
+            answer = max(answer, dp[i][j])
+        else:
+            dp[i][j] = dp[i][j-1]
+
+# for i in range(len(dp)):
+#     print(*dp[i])
 print(N - answer)
 
