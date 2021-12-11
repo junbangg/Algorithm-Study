7a3821b8c9ed94e4f5072b56cc4c468cdd1137d8
junbangg <wnstjr4620@naver.com>
2021-12-12 01:22:41 +0900

    Added Swift substring code

--- Swift Code Snippets/snippets.swift
+++ Swift Code Snippets/snippets.swift
@@ -21,3 +21,52 @@ let counts = Dictionary(mappedItems, uniquingKeyswith: +)
 
 // Count unique values in Array
 return Set(array).count
+
+/// Substrings!!!!
+// Swift4/5
+public extension String {
+  subscript(value: Int) -> Character {
+    self[index(at: value)]
+  }
+}
+
+public extension String {
+  subscript(value: NSRange) -> Substring {
+    self[value.lowerBound..<value.upperBound]
+  }
+}
+
+public extension String {
+  subscript(value: CountableClosedRange<Int>) -> Substring {
+    self[index(at: value.lowerBound)...index(at: value.upperBound)]
+  }
+
+  subscript(value: CountableRange<Int>) -> Substring {
+    self[index(at: value.lowerBound)..<index(at: value.upperBound)]
+  }
+
+  subscript(value: PartialRangeUpTo<Int>) -> Substring {
+    self[..<index(at: value.upperBound)]
+  }
+
+  subscript(value: PartialRangeThrough<Int>) -> Substring {
+    self[...index(at: value.upperBound)]
+  }
+
+  subscript(value: PartialRangeFrom<Int>) -> Substring {
+    self[index(at: value.lowerBound)...]
+  }
+}
+
+private extension String {
+  func index(at offset: Int) -> String.Index {
+    index(startIndex, offsetBy: offset)
+  }
+}
+
+// Usage
+let text = "Hello world"
+text[0] // H
+text[...3] // "Hell"
+text[6..<text.count] // world
+text[NSRange(location: 6, length: 3)] // wor
