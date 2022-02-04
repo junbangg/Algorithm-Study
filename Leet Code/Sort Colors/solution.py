a58e8a969da7bdcd1b3840853970c43a2517270c
junbangg <wnstjr4620@naver.com>
2022-02-04 17:32:59 +0900

    백준: 13975 문제 코드 추가

--- Leet Code/Sort Colors/solution.py
+++ Leet Code/Sort Colors/solution.py
@@ -8,11 +8,11 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         i, j ,k = 0, 0, len(nums)
         while j < k:
-            if nums[j] < 1:
+            if nums[j] == 0:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
                 j += 1
-            elif nums[j] > 1:
+            elif nums[j] == 2:
                 k -= 1
                 nums[j], nums[k] = nums[k], nums[j]
             else:
