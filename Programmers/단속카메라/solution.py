492e59d1c5fa40766bdbf571aefc95aa7226f547
junbangg <wnstjr4620@naver.com>
2022-03-03 23:35:59 +0900

    프로그래머스: 단속 카메라 파이썬 코드 추가

--- Programmers/단속카메라/solution.py
+++ Programmers/단속카메라/solution.py
@@ -1,11 +1,11 @@
 def solution(routes):
-    routes.sort(key = lambda x: x[0], reverse = True)
-    answer, start, end = 0, 0, 0
-    while routes:
-        start, end = routes.pop()
-        while start < end and routes:
-            if routes[-1][0] == start:
-                end = routes.pop()[1]
-            start += 1
+    routes.sort(key = lambda x: x[1])
+    answer = 1
+    endMax = routes[0][1]
+    for start, end in routes:
+        if start <= endMax or end <= endMax:
+            continue
         answer += 1
+        endMax = end
+
     return answer
