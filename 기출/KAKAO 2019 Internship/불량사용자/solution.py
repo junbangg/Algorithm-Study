20613af9823f204d052d487ef94ae447920b1694
junbangg <wnstjr4620@naver.com>
2022-04-12 13:29:32 +0900

    Programmers: 카카오 2019 인턴십 튜플, 불량 사용자 코드 추가

---
+++ 기출/KAKAO 2019 Internship/불량사용자/solution.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+
+def isValidID(candidate_id, banned_id):
+    for i in range(len(candidate_id)):
+        if banned_id[i] == '*':
+            continue
+        if candidate_id[i] != banned_id[i]:
+            return False
+    return True
+
+def isValidCandidate(candidates, banned):
+    for i in range(len(candidates)):
+        candidate_id, banned_id = candidates[i], banned[i]
+        if len(banned_id) != len(candidate_id):
+            return False
+        if not isValidID(candidate_id, banned_id):
+            return False
+    return True
+
+
+def solution(user_id, banned_id):
+    candidates = permutations(user_id, len(banned_id))
+    banned = set()
+
+    for cand in candidates:
+        if isValidCandidate(cand, banned_id):
+            banned.add(''.join(sorted(list(cand))))
+
+    return len(banned)
---
+++ 기출/KAKAO 2019 Internship/튜플/solution.py
@@ -0,0 +1,48 @@
+def getNumbers(string):
+    data = []
+    # 전처리
+    isOpen = False
+    number, buffer = [], []
+    
+    for c in string[1:-1]:
+        if c == '{':
+            isOpen = True
+            continue
+        if isOpen:
+            if c.isdigit():
+                number.append(c)
+            elif c == ',':
+                if number:
+                    buffer.append(int(''.join(number)))
+                number = []
+            else: # }
+                isOpen = False
+                if number:
+                    buffer.append(int(''.join(number)))
+                    number = []
+                data.append(buffer)
+                buffer = []
+    return data
+
+def getTuple(numbers):
+    _set = set()
+    tuple = []
+    for data in sorted(numbers, key = len):
+        unique = set(data) - _set
+        tuple.extend(list(unique))
+        for num in list(unique): _set.add(num)
+    print(tuple)
+    return tuple
+
+def solution(s):
+    print(s)
+    numbers = getNumbers(s)
+    print(numbers)
+    return getTuple(numbers)
+
+
+
+tc1 = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
+tc2 = "{{1,2,3},{2,1},{1,2,4,3},{2}}"
+tc3 = "{{20,111},{111}}"
+print(solution(tc2))
