e801153dac9033bdd1881f9d43df98727385b219
junbangg <wnstjr4620@naver.com>
2022-04-18 16:14:13 +0900

    Programmers: 호텔 방배정 first 최적화

--- 기출/KAKAO 2019 Internship/호텔방배정/solution2.py
+++ 기출/KAKAO 2019 Internship/호텔방배정/solution2.py
@@ -1,7 +1,23 @@
+def find(parents, a):
+    if parents[a] != a:
+        parents[a] = find(parents, parents[a])
+    return parents[a]
 
+def union(parents, a, b):
+    aParent = find(parents, a)
+    bParent = find(parents, b)
+    if aParent < bParent:
+        parents[aParent] = bParent
+    else:
+        parents[bParent] = aParent
 
 def solution(k, room_number):
-    rooms = [0] + [room for room in room_number]
-
-    print(rooms)
+    rooms = [i for i in range(k+1)]
+    answer = []
+    for room in room_number:
+        parent = find(rooms, room)
+        answer.append(parent)
+        if parent < k:
+            union(rooms, parent, parent+1)
+    return answer   
         
