61c7c57824665e7045402bbf921bee04d447cb89
junbangg <wnstjr4620@naver.com>
2022-04-21 20:20:52 +0900

    Programmer: 추가

---
+++ 기출/KAKAO 2019/매칭 점수/solution.py
@@ -0,0 +1,30 @@
+class Page:
+    def __init__(self, url, score, numberOfLinks, linkedPages):
+        self.url = url
+        self.score = score
+        self.numberOfLinks = numberOfLinks
+        self.outputLinkScore = self.getOutputLinkScore(self)
+        self.linkScore = self.getLinkScore(linkedPages)
+        self.matchingScore = self.getMatchingScore(self)
+
+    def getMatchingScore(self):
+        return self.score // self.linkScore
+
+    def getLinkScore(linkedPages):
+        total = 0
+        for page in linkedPages:
+            total += page.outputLinkScore
+        return total
+        
+    def getOutputLinkScore(self):
+        return self.score // self.numberOfLinks
+
+def getWordScore():
+    pass
+
+def solution(word, pages):
+    answer = 0
+    for data in word.split("</html>"):
+        print(data)
+        
+    return answer
