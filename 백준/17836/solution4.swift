5ff5b3551e7112b02618c906ea138c3c1136dd43
junbangg <wnstjr4620@naver.com>
2022-05-31 18:19:05 +0900

    백준: 17836 스위프트 풀이 추가/제거

--- 백준/17836/solution4.swift
+++ 백준/17836/solution4.swift
@@ -4,13 +4,13 @@ func getTime(_ x: Int, _ y: Int, _ nx: Int, _ ny: Int) -> Int {
     return abs(nx - x) + abs(ny - y)
 }
 
-var swordTime = Int.max
-
 func solution(_ N: Int, _ M: Int, _ T: Int, _ board: [[Int]]) -> Int {
     let dx = [1, -1, 0, 0]
     let dy = [0, 0, -1, 1]
     var visited = Array(repeating: Array(repeating: false, count: M), count: N)
     var q: [(Int, Int, Int)] = [(0, 0, 0)]
+    var swordTime = Int.max
+    var swordlessTime = Int.max
     var nx = 0
     var ny = 0
     var counter = 0
@@ -27,7 +27,8 @@ func solution(_ N: Int, _ M: Int, _ T: Int, _ board: [[Int]]) -> Int {
                 continue
             }
             if nx == N-1 && ny == M-1 {
-                return currentTime + 1
+                swordlessTime = min(swordlessTime, currentTime + 1)
+                break
             }
             if board[nx][ny] == 2 {
                 swordTime = currentTime + 1 + getTime(nx, ny, N-1, M-1)
@@ -37,7 +38,7 @@ func solution(_ N: Int, _ M: Int, _ T: Int, _ board: [[Int]]) -> Int {
         }
         counter += 1
     }
-    return Int.max
+    return min(swordlessTime, swordTime)
 }
 
 let NMT = readLine()!.split(separator: " ").compactMap { Int($0) }
@@ -52,6 +53,6 @@ for _ in 0..<N {
     board.append(col)
 }
 
-let time = min(solution(N, M, T, board), swordTime)
+let time = solution(N, M, T, board)
 
 print(time <= T ? time : "Fail")
