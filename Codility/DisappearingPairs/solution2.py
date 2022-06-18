1ef24990ec0c57c097d2dc0ee98a8ced712719e5
junbangg <wnstjr4620@naver.com>
2022-06-18 11:35:23 +0900

    Leet Code: Disappearing Pairs 풀이 추가

---
+++ Codility/DisappearingPairs/solution2.py
@@ -0,0 +1,8 @@
+def solution(S):
+    stack = []
+    for c in S:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    return ''.join(stack)
