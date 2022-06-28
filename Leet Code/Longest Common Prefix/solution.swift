acba46c8c02c10825f960dfe224814ad674af23e
junbangg <wnstjr4620@naver.com>
2022-06-28 11:23:01 +0900

    Longest Common Prefix 풀이 추가

--- Leet Code/Longest Common Prefix/solution.swift
+++ Leet Code/Longest Common Prefix/solution.swift
@@ -9,6 +9,7 @@
 //     if 
 // }
 
+// O(NM)
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
         var answer: String = ""
@@ -38,6 +39,44 @@ class Solution {
             left = mid + 1
         }
         
+        return answer
+    }
+}
+
+// 살짝 최적화
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        var answer: String = ""
+        let minLengthString = strs.min { $0.count < $1.count }
+        guard let minLengthString = minLengthString  else {
+            return ""
+        }
+        var left: Int = 0
+        var right: Int = minLengthString.count
+        var prefixString: String = minLengthString
+
+        first: while left <= right {
+            let mid = left + (right - left) / 2
+
+            if mid > strs[0].count {
+                right = mid - 1
+                continue
+            }
+            prefixString = String(strs[0].prefix(mid))
+            for i in 1..<strs.count {
+                if strs[i].count < mid {
+                    right = mid - 1
+                    continue first
+                }
+                if String(strs[i].prefix(mid)) != prefixString {
+                    right = mid - 1
+                    continue first
+                }
+            }
+            answer = prefixString
+            left = mid + 1
+        }
+
         return answer
     }
 }
